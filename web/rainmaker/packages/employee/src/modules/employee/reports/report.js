5225c42db57f5a711a1c7c9cdcdc2aca13c2edc2
ramanand <ramanandkr21@gmail.com>
2019-10-09 15:14:38 +0530

    report changes

--- web/rainmaker/packages/employee/src/modules/employee/reports/report.js
+++ web/rainmaker/packages/employee/src/modules/employee/reports/report.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { commonApiPost } from "egov-ui-kit/utils/api";
 import SearchForm from "./searchForm";
 import ReportResult from "./reportResult";
-import { getMetaDataUrl, getReportName, options } from "./commons/url";
+import { getMetaDataUrl, options } from "./commons/url";
 import commonConfig from "config/common.js";
 import { Screen } from "modules/common";
 import { getTenantId, setReturnUrl, localStorageGet } from "egov-ui-kit/utils/localStorageUtils";
@@ -46,11 +46,11 @@ class Report extends Component {
     // showTable(false);
     // setReportResult({});
     // setMetaData(mockData);
-    let urlBase = getMetaDataUrl(moduleName);
+    let urlBase = getMetaDataUrl(moduleName,reportName);
 
     //commonApiPost("/report/" + moduleName + "/metadata/_get", {}, { tenantId: tenantId, reportName: "SourceWiseReport" }).then(
     urlBase &&
-      commonApiPost(urlBase, {}, { tenantId: tenantId, reportName: getReportName(moduleName, reportName) }).then(
+      commonApiPost(urlBase, {}, { tenantId: tenantId, reportName: reportName}).then(
         function(response) {
           if (response && response.reportDetails) response.reportDetails.reportName = reportName; //temp soln for custom report name
           setFlag(1);
@@ -71,7 +71,8 @@ class Report extends Component {
 
   render() {
     let { match } = this.props;
-    let needDefaultSearch = options[this.props.match.params.moduleName] ? options[this.props.match.params.moduleName][0].needDefaultSearch : false;
+   // let needDefaultSearch = options[this.props.match.params.moduleName] ? options[this.props.match.params.moduleName][0].needDefaultSearch : false;
+   let needDefaultSearch = false;
     return (
       <Screen>
         <div style={{ margin: "8px" }}>
