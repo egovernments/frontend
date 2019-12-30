ce306b2a81b984036a42d5eb703c0ed755c742ea
gopesh <gopesh.yadav@egovernments.org>
2019-12-30 14:52:40 +0530

    sort based on application date

--- web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/screens/specs/bpastakeholder/searchResource/functions.js
+++ web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/screens/specs/bpastakeholder/searchResource/functions.js
@@ -102,7 +102,7 @@ export const searchApiCall = async (state, dispatch) => {
     const response = await getSearchResults(queryObject);
     const businessIdToOwnerMapping = await getWorkFlowData(response.Licenses);
     try {
-      // response.Licenses.sort((item1, item2) => item1.applicationDate > item2.applicationDate ? -1 : 1)
+      response.Licenses.sort((item1, item2) => item1.applicationDate > item2.applicationDate ? -1 : 1)
       let data = response.Licenses.map(item => ({
         [getTextToLocalMapping("Application No")]:
           item.applicationNumber || "-",
