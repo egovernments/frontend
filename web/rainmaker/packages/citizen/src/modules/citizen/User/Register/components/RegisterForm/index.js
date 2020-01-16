50c5adab34d7768a889c76ccf843c43112e90cd9
karanbk-eGov <karan.bajrang@egovernments.org>
2020-01-16 14:54:06 +0530

    whatsaspp register screen web

--- web/rainmaker/packages/citizen/src/modules/citizen/User/Register/components/RegisterForm/index.js
+++ web/rainmaker/packages/citizen/src/modules/citizen/User/Register/components/RegisterForm/index.js
@@ -4,16 +4,17 @@ import Field from "egov-ui-kit/utils/field";
 import { Button, Card, Image } from "components";
 import Label from "egov-ui-kit/utils/translationNode";
 import { CityPicker } from "modules/common";
+import Hidden from "@material-ui/core/Hidden";
 import { startSMSRecevier } from "egov-ui-kit/utils/commons";
 import logo from "egov-ui-kit/assets/images/logo_black.png";
 import "./index.css";
-
 const RegisterForm = ({ handleFieldChange, form,logoUrl }) => {
   const fields = form.fields || {};
   const submit = form.submit;
   return (
+    <div className="rainmaker-displayInline">
     <Card
-      className="col-sm-offset-4 col-sm-4 user-screens-card "
+      className="cardwidth user-screens-card "
       textChildren={
         <div>
           <div className="rainmaker-displayInline" style={{ justifyContent: "center" }}>
@@ -50,6 +51,22 @@ const RegisterForm = ({ handleFieldChange, form,logoUrl }) => {
         </div>
       }
     />
+    <Hidden smDown>
+     <div className="vl">
+       <div className ="circle">
+       <Label  color="black" fontSize= "16px" label="OR"/>
+       </div>
+    </div>
+    <div className="qrscan">
+      <div className="qrImg">
+       <Image className="qrlogo" source="https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjp_P6X1IfnAhUjzzgGHU9AB1kQjRx6BAgBEAQ&url=https%3A%2F%2Fappadvice.com%2Fapp%2Fqr-code-qr-reader-scanner%2F1180506724&psig=AOvVaw3fHo2gA8TxuCpGZ86ywuWt&ust=1579248181278968" />
+       </div>
+       <div  className="qrtext">
+       <Label  color="black" fontSize= "14px" label="Scan the QR Code to continue with Whatsapp"/>
+       </div>
+    </div>
+    </Hidden>
+    </div>
   );
 };
 
