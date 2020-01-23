ecb63cabdfd5f48f5074d6d1d2774f19926b9f83
muralimgwl <murali.m@goodworklabs.com>
2020-01-23 20:55:40 +0530

    try catch block for component

--- web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-molecules/CommonView/index.js
+++ web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-molecules/CommonView/index.js
@@ -5,6 +5,20 @@ import { LoadingIndicator } from "../../ui-molecules";
 import "./index.css";
 
 class CommonView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    })
+    // You can also log error messages to an error reporting service here
+  }
+
   render() {
     const {
       components,
@@ -17,6 +31,13 @@ class CommonView extends React.Component {
       spinner
     } = this.props;
     const { error, message, open } = toastObject;
+    if (this.state.errorInfo) {
+      // Error path
+      console.error("Egov-ui-framework-error",this.state.error && this.state.error.toString());
+      console.error("Egov-ui-framework-errorInfo",this.state.errorInfo.componentStack);
+
+      return null;
+    }
     return (
       <div>
         <RenderScreen
