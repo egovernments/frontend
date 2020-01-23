1bb10d12261b736d9ade815de9808e9f19abff5e
muralimgwl <murali.m@goodworklabs.com>
2020-01-23 11:32:52 +0530

    testing-enabled cash mode rest disabled

--- web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/payResource/capture-payment-details.js
+++ web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/payResource/capture-payment-details.js
@@ -28,16 +28,16 @@ const capturePaymentDetails = getCommonGrayCard({
           tabIcon: "Dashboard",
           tabContent: { cash }
         },
-        {
-          tabButton: "COMMON_CHEQUE",
-          tabIcon: "Schedule",
-          tabContent: { cheque }
-        },
-        {
-          tabButton: "COMMON_DD",
-          tabIcon: "Schedule",
-          tabContent: { demandDraft }
-        },
+        // {
+        //   tabButton: "COMMON_CHEQUE",
+        //   tabIcon: "Schedule",
+        //   tabContent: { cheque }
+        // },
+        // {
+        //   tabButton: "COMMON_DD",
+        //   tabIcon: "Schedule",
+        //   tabContent: { demandDraft }
+        // },
         // {
         //   tabButton: "COMMON_CREDIT_DEBIT_CARD",
         //   tabIcon: "Schedule",
