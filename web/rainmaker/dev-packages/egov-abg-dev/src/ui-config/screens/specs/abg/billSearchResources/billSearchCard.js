b37c4b567080d31bc7f87c1a512b0c39547d1f70
Shreya B <shreya.b@egovernments.org>
2020-01-24 12:32:00 +0530

    Reverted the billingPeriod

--- web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/abg/billSearchResources/billSearchCard.js
+++ web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/abg/billSearchResources/billSearchCard.js
@@ -91,6 +91,10 @@ export const billSearchCard = getCommonCard({
         xs: 12,
         sm: 4
       },
+      localePrefix : {
+        moduleName : "BillingService",
+        masterName : "BusinessService"
+      },
       sourceJsonPath: "searchScreenMdmsData.BillingService.BusinessService",
       beforeFieldChange :(action, state, dispatch) => {
         const labelName = {
