85d5f859322310c792c2cb2a118f6a34ae42fe82
Ashok <ashok.kumar@egovernments.org>
2020-01-29 14:47:14 +0530

    Description change in upload page

--- web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-molecules-local/DocumentList/index.js
+++ web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-molecules-local/DocumentList/index.js
@@ -217,13 +217,15 @@ class DocumentList extends Component {
                     {document.required && (
                       <sup style={{ color: "#E54D42" }}>*</sup>
                     )}
-                       <Typography variant="caption">
+                    {
+                      document.statement && <Typography variant="caption">
                         <LabelContainer
                           labelName={document.statement}
                           labelKey={document.statement}
                         />
                       {/* {document.statement} */}
                       </Typography>
+                    }
                       <Typography variant="caption">
                         <LabelContainer
                           labelName={description.labelName}
