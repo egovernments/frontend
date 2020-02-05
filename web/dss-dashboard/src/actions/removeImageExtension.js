daac0bcf8c1a90d20b7426f627d84eda6b761257
meghaMS <meghana.suresh@tarento.com>
2020-02-05 14:44:36 +0530

    Code refactoring

--- web/dss-dashboard/src/actions/removeImageExtension.js
+++ web/dss-dashboard/src/actions/removeImageExtension.js
@@ -1,6 +1,5 @@
 import CONFIGS from '../config/configs';
 export default function removeImageExtension(image) {
-  console.log('image in removeImageExt file = ', image)
   /*for(var i=0;i<CONFIGS.DOC_EXTENSION.length;i++){
       image = image.replace(CONFIGS.DOC_EXTENSION[i],'')
   }*/
