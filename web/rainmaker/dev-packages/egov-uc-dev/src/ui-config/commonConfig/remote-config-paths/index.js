60290ed2c7924b42d0809aeaa593d0e3aa04c88d
muralimgwl <murali.m@goodworklabs.com>
2020-02-17 12:39:58 +0530

    script update

--- web/rainmaker/dev-packages/egov-uc-dev/src/ui-config/commonConfig/remote-config-paths/index.js
+++ web/rainmaker/dev-packages/egov-uc-dev/src/ui-config/commonConfig/remote-config-paths/index.js
@@ -2,7 +2,18 @@ const remoteConfigPath = (path, screenKey) => {
   let config = {};
   switch (path) {
     default:
-      config = require(`ui-config/screens/specs/${path}/${screenKey}`).default;
+      try {
+        // config=import(`ui-config/screens/specs/${path}/${screenKey}`);
+        // .then(screen => {
+        //   console.log(screen);
+        //   return screen.default;
+        // });
+        // console.log("import",import(`ui-config/screens/specs/${path}/${screenKey}`).then(screen => screen.default));
+        // config=import(`ui-config/screens/specs/${path}/${screenKey}`).default;
+        config=require(`ui-config/screens/specs/${path}/${screenKey}`).default;
+      } catch (e) {
+        console.log(e);
+      }
       break;
   }
   return config;
