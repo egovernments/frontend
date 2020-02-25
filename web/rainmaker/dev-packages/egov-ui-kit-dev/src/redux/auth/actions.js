20efbc89ab3ba5fb7313b51966b7977d3a33cf8d
Ashok <ashok.kumar@egovernments.org>
2020-02-25 16:44:54 +0530

    change-2

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/redux/auth/actions.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/redux/auth/actions.js
@@ -135,6 +135,13 @@ export const logout = () => {
       const authToken = getAccessToken();
       if (authToken) {
         const response = await httpRequest(AUTH.LOGOUT.URL, AUTH.LOGOUT.ACTION, [{ key: "access_token", value: authToken }]);
+        if(process.env="Development"){
+          process.env.REACT_APP_NAME === "Citizen"
+          ? window.location.replace(`${window.basename}/user/register`)
+          : window.location.replace(`${window.basename}/user/login`);
+        }else {
+          window.location.replace(window.origin + "/common");
+        }
       } else {
         clearUserDetails();
         process.env.REACT_APP_NAME === "Citizen"
