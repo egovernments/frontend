32b5a3edf09e9e7f611cf438151bffa24373eb23
Shreya B <shreya.b@egovernments.org>
2020-03-13 14:01:38 +0530

    Payment Ack page configured

--- web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/acknowledgement.js
+++ web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/acknowledgement.js
@@ -7,6 +7,8 @@ import { ifUserRoleExists } from "../utils";
 import {download} from  "../../../../ui-utils/commons";
 import {getHeader} from "./pay"
 import './index.css';
+
+
 const downloadprintMenu=(state,applicationNumber,tenantId)=>{
       let receiptDownloadObject = {
         label: { labelName: "DOWNLOAD RECEIPT", labelKey: "COMMON_DOWNLOAD_RECEIPT" },
@@ -33,18 +35,6 @@ const downloadprintMenu=(state,applicationNumber,tenantId)=>{
       };
     let downloadMenu = [];
     let printMenu = [];
-    // switch (purpose) {
-    //   case "approve":
-    //     downloadMenu = [certificateDownloadObject ];
-    //     printMenu = [certificatePrintObject ];
-    //     break;
-    //   case "apply":
-    //     downloadMenu = [applicationDownloadObject];
-    //     printMenu = [applicationPrintObject];
-    //     break;
-    //     default:
-    //     break;
-    // }
         downloadMenu = [receiptDownloadObject];
         printMenu = [receiptPrintObject];
     
@@ -86,12 +76,6 @@ const downloadprintMenu=(state,applicationNumber,tenantId)=>{
           }
     
         },
-        // gridDefination: {
-        //   xs: 12,
-        //   sm: 6
-        // }
-      
-  
       }
      
   }
@@ -104,35 +88,13 @@ const getAcknowledgementCard = (
     tenant
 ) => {
     const roleExists = ifUserRoleExists("CITIZEN");
-    //console.log("=====>",state)
-    // const commonPayDetails = get(state.screenConfiguration.preparedFinalObject , "businessServiceMdmsData.common-masters.uiCommonPay");
-    // const businessServiceCode = get(state.screenConfiguration.preparedFinalObject , "businessServiceInfo.code")
     let header = getHeader(state);
-    // commonPayDetails && commonPayDetails.map(item => {
-    //     if (item.code == businessServiceCode) {
-    //         header  = getHeader(item.headerBandLabel,consumerCode);
-    //     }
-    // })
+    const commonPayDetails = get(state.screenConfiguration.preparedFinalObject , "businessServiceMdmsData.common-masters.uiCommonPay");
+    const businessServiceCode = get(state.screenConfiguration.preparedFinalObject , "businessServiceInfo.code");
+    const serviceDetails = commonPayDetails && commonPayDetails.length > 0 && commonPayDetails.filter(item => item.code === businessServiceCode)
+    const {citizenSuccess , employeeSuccess , citizenFailure , employeeFailure} = serviceDetails[0];
     if (status === "success") {
         return {
-            // header: getCommonContainer({
-            //     header: getCommonHeader({
-            //         labelName: roleExists ? 'Payment Details' : 'Collection Details',
-            //         labelKey: roleExists ? "PAYMENT_HEADER_CITIZEN" : "PAYMENT_HEADER_EMPLOYEE"
-            //     }),
-            //     applicationNumber: {
-            //         uiFramework: "custom-atoms-local",
-            //         moduleName: "egov-common",
-            //         componentPath: "ApplicationNoContainer",
-            //         props: {
-            //             number: consumerCode,
-            //             label: {
-            //                 labelValue:"Consumer Code.:",
-            //                 labelKey:"PAYMENT_COMMON_CONSUMER_CODE"
-            //             }
-            //         }                  
-            //     }
-            // }),
             header,
             headerdownloadprint:downloadprintMenu(state,receiptNumber,tenant),
             applicationSuccessCard: {
@@ -143,16 +105,13 @@ const getAcknowledgementCard = (
                         icon: "done",
                         backgroundColor: "#39CB74",
                         header: {
-                            labelName: roleExists ? "Payment has been paid successfully!" : "Payment has been collected successfully!",
-                            labelKey: roleExists ? "PAYMENT_MESSAGE_CITIZEN" : "PAYMENT_MESSAGE_EMPLOYEE"
+                            labelKey: roleExists ? citizenSuccess.primaryMessage : employeeSuccess.primaryMessage
                         },
                         body: {
-                            labelName: roleExists ? "A notification regarding Payment has been sent to property owner at registered Mobile No." : "A notification regarding Payment Collection has been sent to property owner at registered Mobile No.",
-                            labelKey: roleExists ? "PAYMENT_MESSAGE_DETAIL_CITIZEN" : "PAYMENT_MESSAGE_DETAIL_EMPLOYEE"
+                            labelKey: roleExists ? citizenSuccess.secondaryMessage : employeeSuccess.secondaryMessage
                         },
                         tailText: {
-                            labelName: "Payment Receipt No.",
-                            labelKey: "PAYMENT_RECEIPT_NO"
+                            labelKey : roleExists ? citizenSuccess.receiptNo : employeeSuccess.receiptNo
                         },
                         number: receiptNumber
                     })
@@ -171,12 +130,10 @@ const getAcknowledgementCard = (
                         icon: "close",
                         backgroundColor: "#E54D42",
                         header: {
-                            labelName: "Payment has failed!",
-                            labelKey: "PAYMENT_FAILURE_MESSAGE"
+                            labelKey: roleExists ? citizenFailure.primaryMessage : employeeFailure.primaryMessage
                         },
                         body: {
-                            labelName: "A notification regarding payment failure has been sent to property owner at registered Mobile No.",
-                            labelKey: "PAYMENT_FAILURE_MESSAGE_DETAIL"
+                            labelKey: roleExists ? citizenFailure.secondaryMessage : employeeFailure.secondaryMessage
                         }
                     })
                 }
