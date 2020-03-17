a37633df498ddd87e44a9701d64d3e416b1b66db
Satyam-egov-tarento <56678653+Satyam-egov-tarento@users.noreply.github.com>
2020-03-17 07:28:58 +0530

    Fix for Sheet issue 74, 75 (#834)

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/applyResource/review-owner.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/applyResource/review-owner.js
@@ -6,7 +6,7 @@ import {
   getLabel,
 } from "egov-ui-framework/ui-config/screens/specs/utils";
 import { getQueryArg } from "egov-ui-framework/ui-utils/commons";
-import { convertEpochToDate } from "../../utils";
+import { convertEpochToDateAndHandleNA } from "../../utils";
 
 const service = getQueryArg(window.location.href, "service")
 
@@ -177,7 +177,7 @@ export const reviewConnectionExecutionDate = getLabelWithValue(
   },
   {
     jsonPath: "WaterConnection[0].connectionExecutionDate",
-    callBack: convertEpochToDate
+    callBack: convertEpochToDateAndHandleNA
   }
 );
 export const reviewMeterId = getLabelWithValue(
@@ -195,7 +195,7 @@ export const reviewMeterInstallationDate = getLabelWithValue(
   },
   {
     jsonPath: "WaterConnection[0].meterInstallationDate",
-    callBack: convertEpochToDate
+    callBack: convertEpochToDateAndHandleNA
   }
 );
 
