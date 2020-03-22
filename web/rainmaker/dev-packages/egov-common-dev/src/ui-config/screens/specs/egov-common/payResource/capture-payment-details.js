9e45e4d659d3fefdff5233f0529e784d6ed7b2a1
Shreya B <shreya.b@egovernments.org>
2020-03-22 23:13:07 +0530

    Fixed Credit/Debit issue

--- web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/payResource/capture-payment-details.js
+++ web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/payResource/capture-payment-details.js
@@ -4,32 +4,6 @@ import {
 } from "egov-ui-framework/ui-config/screens/specs/utils";
 import { cash, demandDraft, cheque, card } from "./payment-methods";
 
-const tabs= [
-  {
-    code : "CASH",
-    tabButton: "COMMON_CASH",    
-    tabIcon: "Dashboard",
-    tabContent: { cash }
-  },
-  {
-    code : "CHEQUE",
-    tabButton: "COMMON_CHEQUE",
-    tabIcon: "Schedule",
-    tabContent: { cheque }
-  },
-  {
-    code : "DD",
-    tabButton: "COMMON_DD",
-    tabIcon: "Schedule",
-    tabContent: { demandDraft }
-  },
-  {
-    code : "CARD",
-    tabButton: "COMMON_CREDIT_DEBIT_CARD",
-    tabIcon: "Schedule",
-    tabContent: { card }
-  }
-]
 
 const capturePaymentDetails = getCommonGrayCard({
   header: getCommonSubHeader(
@@ -45,7 +19,7 @@ const capturePaymentDetails = getCommonGrayCard({
     moduleName: "egov-common",
     componentPath: "CustomTabContainer",
     props: {
-      tabs,
+      // tabs,
       jsonPath : "businessServiceInfo"
     },
     type: "array"
