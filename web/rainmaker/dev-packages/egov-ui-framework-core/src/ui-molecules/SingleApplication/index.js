4bfb37e1a1e16886d00c3310f9bdb0e644ca7dd7
Shreya B <shreya.b@egovernments.org>
2020-03-29 15:48:53 +0530

    Fixed citizen card button

--- web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-molecules/SingleApplication/index.js
+++ web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-molecules/SingleApplication/index.js
@@ -217,7 +217,7 @@ class SingleApplication extends React.Component {
                       // setRoute(url);
                     }}>
                       <Label
-                        labelKey={(item.status === "APPROVED" || item.status === "EXPIRED") && moduleName === "TL" ? "TL_VIEW_DETAILS_RENEWAL" : "TL_VIEW_DETAILS"}
+                        labelKey={"TL_VIEW_DETAILS"}
                         textTransform={"uppercase"}
                         style={{
                           color: "#fe7a51",
