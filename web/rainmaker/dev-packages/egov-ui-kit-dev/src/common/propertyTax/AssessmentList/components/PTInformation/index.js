6bbcdc496381146e14d43028397649e1cfc77df4
Karan Bajrang <53563693+karanbk-eGov@users.noreply.github.com>
2020-04-02 16:16:20 +0530

    Ptimg (#964)
    * image applc
    
    * due amount removed
    
    * pdffromdiv

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/AssessmentList/components/PTInformation/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/AssessmentList/components/PTInformation/index.js
@@ -136,7 +136,9 @@ class PTInformation extends React.Component {
     let corpCity = "";
     let ulbGrade = "";
     if (get(properties, "tenantId")) {
-      logoUrl = get(properties, "tenantId") ? this.getLogoUrl(get(properties, "tenantId")) : "";
+      let tenantid=get(properties, "tenantId");
+     // logoUrl = get(properties, "tenantId") ? this.getLogoUrl(get(properties, "tenantId")) : "";
+     logoUrl = window.location.origin + `/pb-egov-assets/${tenantid}/logo.png`;
       corpCity = `TENANT_TENANTS_${get(properties, "tenantId").toUpperCase().replace(/[.:-\s\/]/g, "_")}`;
       const selectedCityObject = cities && cities.length > 0 && cities.filter(item => item.code === get(properties, "tenantId"));
       ulbGrade = selectedCityObject ? `ULBGRADE_${get(selectedCityObject[0], "city.ulbGrade")}` : "MUNICIPAL CORPORATION";
@@ -181,7 +183,7 @@ class PTInformation extends React.Component {
                     style={{ display: "flex", backgroundColor: "rgb(242, 242, 242)", minHeight: "120px", alignItems: "center", paddingLeft: "10px" }}
                     textChildren={
                       <div style={{ display: "flex" }}>
-                        {/* <Image  id="image-id" style={logoStyle} source={logoUrl} /> */}
+                        <Image  id="image-id" style={logoStyle} source={logoUrl} />
                         <div style={{ marginLeft: 30 }}>
                           <div style={{ display: "flex", marginBottom: 5 }}>
                             <Label label={corpCity} fontSize="20px" fontWeight="500" color="rgba(0, 0, 0, 0.87)" containerStyle={{ marginRight: 10, textTransform: "uppercase" }} />
