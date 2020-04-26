42897b96be6f3d7017fd7cec253deebf4d47c425
Satyam-egov-tarento <56678653+Satyam-egov-tarento@users.noreply.github.com>
2020-04-26 14:40:33 +0530

    Added fix for WOR-232 (#1088)

--- web/rainmaker/dev-packages/egov-pt-dev/src/ui-config/screens/specs/pt-common-screens/register-property.js
+++ web/rainmaker/dev-packages/egov-pt-dev/src/ui-config/screens/specs/pt-common-screens/register-property.js
@@ -48,73 +48,73 @@ const getMDMSPropertyData = async (dispatch) => {
         {
           moduleName: "PropertyTax",
           masterDetails: [
-          {name: "PropertyType"},
-          {name: "UsageCategory"},
-          {name:"UsageCategoryMajor"},
-          {name:"UsageCategoryMinor"},
-          {name:"UsageCategorySubMinor"}
+            { name: "PropertyType" },
+            { name: "UsageCategory" },
+            { name: "UsageCategoryMajor" },
+            { name: "UsageCategoryMinor" },
+            { name: "UsageCategorySubMinor" }
           ]
         }
       ]
     }
   }
   try {
-    let payload=null;
-     payload = await httpRequest("post","/egov-mdms-service/v1/_search","_search",[],mdmsBody);
-  let PropertyType=[];let subUsageType=[]; let UsageType=[];let Commercial=[];let Industrial=[]; let Institutional=[]; let Mixed=[];
-  console.log("===>payload",payload)
-  payload.MdmsRes.PropertyTax.PropertyType.filter(item=>{
-    if(item.name!="Built Up"){
-      PropertyType.push({
-        name:item.name,
-        code: item.code,
-        isActive: item.active
-      })
-    }
-    
-  })
-payload.MdmsRes.PropertyTax.PropertyType=PropertyType;
+    let payload = null;
+    payload = await httpRequest("post", "/egov-mdms-service/v1/_search", "_search", [], mdmsBody);
+    let PropertyType = []; let subUsageType = []; let UsageType = []; let Commercial = []; let Industrial = []; let Institutional = []; let Mixed = [];
+    console.log("===>payload", payload)
+    payload.MdmsRes.PropertyTax.PropertyType.filter(item => {
+      if (item.name != "Built Up") {
+        PropertyType.push({
+          name: item.name,
+          code: item.code,
+          isActive: item.active
+        })
+      }
 
-payload.MdmsRes.PropertyTax.UsageCategory.forEach(item=>{
-  if(item.code.split(".").length<=2 && item.code!="NONRESIDENTIAL"){
-      UsageType.push({
-        active:item.active,
-        name:item.name,
-        code:item.code,
-        fromFY:item.fromFY
-      })
-    }
-})
-payload.MdmsRes.PropertyTax.UsageType=UsageType;
+    })
+    payload.MdmsRes.PropertyTax.PropertyType = PropertyType;
 
-payload.MdmsRes.PropertyTax.UsageCategory.forEach(item=>{
-  if(item.code.split(".").includes("COMMERCIAL") && item.code.split(".").length>2){
-    Commercial.push({
-      active:item.active,
-        name:item.name,
-        code:item.code,
-        fromFY:item.fromFY
+    payload.MdmsRes.PropertyTax.UsageCategory.forEach(item => {
+      if (item.code.split(".").length <= 2 && item.code != "NONRESIDENTIAL") {
+        UsageType.push({
+          active: item.active,
+          name: item.name,
+          code: item.code,
+          fromFY: item.fromFY
+        })
+      }
     })
-  }else if(item.code.split(".").includes("INDUSTRIAL") && item.code.split(".").length>2){
-    Industrial.push({
-      active:item.active,
-        name:item.name,
-        code:item.code,
-        fromFY:item.fromFY
+    payload.MdmsRes.PropertyTax.UsageType = UsageType;
+
+    payload.MdmsRes.PropertyTax.UsageCategory.forEach(item => {
+      if (item.code.split(".").includes("COMMERCIAL") && item.code.split(".").length > 2) {
+        Commercial.push({
+          active: item.active,
+          name: item.name,
+          code: item.code,
+          fromFY: item.fromFY
+        })
+      } else if (item.code.split(".").includes("INDUSTRIAL") && item.code.split(".").length > 2) {
+        Industrial.push({
+          active: item.active,
+          name: item.name,
+          code: item.code,
+          fromFY: item.fromFY
+        })
+      } else if (item.code.split(".").includes("INSTITUTIONAL") && item.code.split(".").length > 2) {
+        Institutional.push({
+          active: item.active,
+          name: item.name,
+          code: item.code,
+          fromFY: item.fromFY
+        })
+      }
     })
-  }else if(item.code.split(".").includes("INSTITUTIONAL") && item.code.split(".").length>2){
-    Institutional.push({
-      active:item.active,
-        name:item.name,
-        code:item.code,
-        fromFY:item.fromFY
-    })
-  }
-})
-payload.MdmsRes.PropertyTax.Commercial=Commercial;
-payload.MdmsRes.PropertyTax.Industrial=Industrial;
-payload.MdmsRes.PropertyTax.Institutional=Institutional;
-// payload.MdmsRes.PropertyTax.Institutional=Mixed;
+    payload.MdmsRes.PropertyTax.Commercial = Commercial;
+    payload.MdmsRes.PropertyTax.Industrial = Industrial;
+    payload.MdmsRes.PropertyTax.Institutional = Institutional;
+    // payload.MdmsRes.PropertyTax.Institutional=Mixed;
 
 
     dispatch(prepareFinalObject("searchScreenMdmsData", payload.MdmsRes));
@@ -138,7 +138,6 @@ const getMdmsData = async (action, state, dispatch) => {
           moduleName: "egov-location",
           masterDetails: [
             {
-              // hierarchyTypeCode=REVENUE&boundaryType=Locality
               name: "TenantBoundary"
             }
           ]
@@ -164,8 +163,6 @@ const getMdmsData = async (action, state, dispatch) => {
       mdmsBody
     );
 
-    console.log("==payload",payload);
-
     let OwnerShipCategory = get(
       payload,
       "MdmsRes.common-masters.OwnerShipCategory"
@@ -186,7 +183,7 @@ const getMdmsData = async (action, state, dispatch) => {
     
     payload.MdmsRes['common-masters'].Institutions = institutions;
     payload.MdmsRes['common-masters'].OwnerShipCategory = OwnerShipCategory;
-    payload.MdmsRes['common-masters'].mollaha = 
+    // payload.MdmsRes['common-masters'].mollaha = 
     dispatch(prepareFinalObject("applyScreenMdmsData", payload.MdmsRes));
   } catch (e) {
     console.log(e);
@@ -219,37 +216,11 @@ const screenConfig = {
       )
     );
 
-    set(
-      action.screenConfig,
-      "components.div.children.formwizardFirstStep.children.transferorDetails.children.cardContent.children.cardOne.props.scheama.children.cardContent.children.ownerContainer.children.ownerSpecialDocumentID.props.style.display",
-      'none'
-    );
-    set(
-      action.screenConfig,
-      "components.div.children.formwizardFirstStep.children.transferorDetails.children.cardContent.children.cardOne.props.scheama.children.cardContent.children.ownerContainer.children.ownerSpecialDocumentType.props.style.display",
-      'none'
-    );
-
     //Set Module Name
     set(state, "screenConfiguration.moduleName", "egov-pt");
 
     // Set MDMS Data
     getMdmsData(action, state, dispatch).then(() => {
-      // Set Dropdowns Data
-      let buildingUsageTypeData = get(
-        state,
-        "screenConfiguration.preparedFinalObject.applyScreenMdmsData.firenoc.BuildingType",
-        []
-      );
-      buildingUsageTypeData = getFirstListFromDotSeparated(
-        buildingUsageTypeData
-      );
-      dispatch(
-        prepareFinalObject(
-          "applyScreenMdmsData.DropdownsData.BuildingUsageType",
-          buildingUsageTypeData
-        )
-      );
       let ownershipCategory = get(
         state,
         "screenConfiguration.preparedFinalObject.applyScreenMdmsData.common-masters.OwnerShipCategory",
@@ -263,40 +234,6 @@ const screenConfig = {
         )
       );
     });
-
-    // Set defaultValues of radiobuttons and selectors
-    let noOfBuildings = get(
-      state,
-      "screenConfiguration.preparedFinalObject.FireNOCs[0].fireNOCDetails.noOfBuildings",
-      "SINGLE"
-    );
-    set(
-      state,
-      "screenConfiguration.preparedFinalObject.FireNOCs[0].fireNOCDetails.noOfBuildings",
-      noOfBuildings
-    );
-    let nocType = get(
-      state,
-      "screenConfiguration.preparedFinalObject.FireNOCs[0].fireNOCDetails.fireNOCType",
-      "PROVISIONAL"
-    );
-    set(
-      state,
-      "screenConfiguration.preparedFinalObject.FireNOCs[0].fireNOCDetails.fireNOCType",
-      nocType
-    );
-    if (
-      get(
-        state,
-        "screenConfiguration.preparedFinalObject.FireNOCs[0].fireNOCDetails.fireNOCType"
-      ) === "PROVISIONAL"
-    ) {
-      set(
-        action.screenConfig,
-        "components.div.children.formwizardFirstStep.children.nocDetails.children.cardContent.children.nocDetailsContainer.children.provisionalNocNumber.props.style",
-        { visibility: "hidden" }
-      );
-    }
 
     return action;
   },
