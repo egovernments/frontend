7ac9c659b6a04f8bc2394ca00b231ae2b4aec93d
Karan Bajrang <53563693+karanbk-eGov@users.noreply.github.com>
2020-04-30 18:23:24 +0530

    Ux changes (#1137)
    * page
    
    * search city autofocus + lang button hover
    
    * SLA days remaining style fix
    
    * register page new design

--- web/rainmaker/packages/citizen/src/modules/citizen/User/Register/components/RegisterForm/index.js
+++ web/rainmaker/packages/citizen/src/modules/citizen/User/Register/components/RegisterForm/index.js
@@ -15,6 +15,7 @@ const RegisterForm = ({ handleFieldChange, form,logoUrl ,qrCodeURL,enableWhatsAp
   const submit = form.submit;
   return (
     <div className="rainmaker-displayInline">
+        {!enableWhatsApp&&
     <Card
       className={enableWhatsApp?"register-cardwidth user-screens-card":"col-sm-offset-4 col-sm-4 user-screens-card"}
       textChildren={
@@ -68,8 +69,105 @@ const RegisterForm = ({ handleFieldChange, form,logoUrl ,qrCodeURL,enableWhatsAp
         </div>
       }
     />
+    }
+    {enableWhatsApp&&
+    
+        <Hidden mdUp>
+   <Card
+      className={enableWhatsApp?"register-cardwidth user-screens-card":"col-sm-offset-4 col-sm-4 user-screens-card"}
+      textChildren={
+        <div>
+          <div className="rainmaker-displayInline" style={{ justifyContent: "center" }}>
+            <div style={{ marginBottom: "24px" }}>
+              <Image className="mseva-logo" source={`${logo}`} />
+            </div >
+          <div style={{marginLeft:"7px", marginBottom: "24px" }}>
+          <Label bold={true}  fontSize= "23px" label="|" />
+          </div>
+           <div style={{marginLeft:"7px" }}>
+              <Label bold={true} color="black" fontSize= "24px" label="STATE_LABEL" />
+           </div>
+          </div>
+          <Label className="heading text-center" bold={true} dark={true} fontSize={16} label="CORE_REGISTER_HEADING" />
+          <Field fieldKey="phone" field={fields.phone} handleFieldChange={handleFieldChange} />
+          <Field fieldKey="name" field={fields.name} handleFieldChange={handleFieldChange} />
+          <CityPicker onChange={handleFieldChange} fieldKey="city" field={fields.city} />
+          <div style={{ marginBottom: "24px", position: "relative", zIndex: 10 }} className="text-right">
+            <Label id="otp-trigger" className="otp-prompt" label="CORE_REGISTER_HAVE_ACCOUNT" />
+            <Link to="/user/login">
+              <div style={{ display: "inline-block" }}>
+                <Label containerStyle={{ cursor: "pointer" }} id="otp-resend" className="otp-resend" label="CORE_COMMON_LOGIN" />
+              </div>
+            </Link>
+          </div>
+          <Button
+            primary={true}
+            fullWidth={true}
+            {...submit}
+            onClick={(e) => {
+              startSMSRecevier();
+            }}
+          />
+         
+          <div>
+        <div className="register-hl-divider">
+       <div className ="register-circle-mobile">
+       <Label  color="black" fontSize= "16px" label="Or"/>
+       </div>
+    </div>
+    <div className="rainmaker-displayInline register-mobile-whatsapp-button" onClick={()=>{window.location.href="https://api.whatsapp.com/send?phone=918744960111&text=mSeva-send+this+to+start"}} >      
+        <Icon action="custom" name="whatsapp" className="register-mobile-whatsapp-icon" />
+        <Label bold={true} color="black" fontSize= "14px" label="WHATSAPP_CONTINUE_MOBILE"/>
+    </div>
+    </div>
+     
+        </div>
+      }
+    />
+      </Hidden>
+      }
+
      {enableWhatsApp&&
     <Hidden smDown>
+        <Card
+     className="wha-user-screen-card"
+      textChildren={
+        <div >
+          <div className="rainmaker-displayInline" style={{ justifyContent: "center" }}>
+            <div style={{ marginBottom: "24px" }}>
+              <Image className="mseva-logo" source={`${logo}`} />
+            </div >
+          <div style={{marginLeft:"7px", marginBottom: "24px" }}>
+          <Label bold={true}  fontSize= "23px" label="|" />
+          </div>
+           <div style={{marginLeft:"7px" }}>
+              <Label bold={true} color="black" fontSize= "24px" label="STATE_LABEL" />
+           </div>
+          </div>
+          <div className="rainmaker-displayInline" style={{ justifyContent: "center" }}>
+          <div style={{ width: "50%",marginTop:"4%"}}>
+          <Label className="heading text-center" bold={true} dark={true} fontSize={24} label="CORE_REGISTER_HEADING" />
+          <Field fieldKey="phone" field={fields.phone} handleFieldChange={handleFieldChange} />
+          <Field fieldKey="name" field={fields.name} handleFieldChange={handleFieldChange} />
+          <CityPicker onChange={handleFieldChange} fieldKey="city" field={fields.city} />
+          <div style={{ marginBottom: "24px", position: "relative", zIndex: 10 }} className="text-right">
+            <Label id="otp-trigger" className="otp-prompt" label="CORE_REGISTER_HAVE_ACCOUNT" />
+            <Link to="/user/login">
+              <div style={{ display: "inline-block" }}>
+                <Label containerStyle={{ cursor: "pointer" }} id="otp-resend" className="otp-resend" label="CORE_COMMON_LOGIN" />
+              </div>
+            </Link>
+          </div>
+          <Button
+            primary={true}
+           fullWidth={true}
+            {...submit}
+            onClick={(e) => {
+              startSMSRecevier();
+            }}
+          />
+     </div>
+    
      <div className="register-vl-divider">
        <div className ="register-circle-web">
        <Label  color="black" fontSize= "16px" label="OR"/>
@@ -82,6 +180,11 @@ const RegisterForm = ({ handleFieldChange, form,logoUrl ,qrCodeURL,enableWhatsAp
        <Label  color="black" fontSize= "14px" label="WHATSAPP_SCAN_QR_CODE"/>
        </div>
     </div>
+  </div>
+    </div>
+      }
+    />
+
     </Hidden>
 }
     </div>
