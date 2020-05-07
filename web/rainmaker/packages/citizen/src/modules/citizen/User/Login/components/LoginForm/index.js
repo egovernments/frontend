ad10566183dc92597c23de61a14867853076324f
Karan Bajrang <53563693+karanbk-eGov@users.noreply.github.com>
2020-05-07 10:32:40 +0530

    login Screen new Design (#1163)

--- web/rainmaker/packages/citizen/src/modules/citizen/User/Login/components/LoginForm/index.js
+++ web/rainmaker/packages/citizen/src/modules/citizen/User/Login/components/LoginForm/index.js
@@ -14,6 +14,7 @@ const LoginForm = ({ handleFieldChange, form, logoUrl,qrCodeURL,enableWhatsApp }
 
   return (
     <div className="rainmaker-displayInline">
+     {!enableWhatsApp&&
     <Card
       className={enableWhatsApp?"login-cardwidth user-screens-card":"login-cardwidthmob col-sm-offset-4 col-sm-4 user-screens-card"}
       textChildren={
@@ -65,8 +66,101 @@ const LoginForm = ({ handleFieldChange, form, logoUrl,qrCodeURL,enableWhatsApp }
         </div>
       }
     />
+}
+{enableWhatsApp&&
+    
+    <Hidden mdUp>
+         <Card
+      className={enableWhatsApp?"login-cardwidth user-screens-card":"login-cardwidthmob col-sm-offset-4 col-sm-4 user-screens-card"}
+      textChildren={
+        <div>
+          <div className="rainmaker-displayInline" style={{ justifyContent: "center" }}>
+            <div style={{ marginBottom: "24px" }}>
+              <Image className="mseva-logo" source={`${logo}`} />
+            </div >
+          <div style={{marginLeft:"7px", marginBottom: "24px" }}>
+          <Label bold={true}  fontSize= "23px" label="|" />
+          </div>
+           <div style={{marginLeft:"7px" }}>
+              <Label bold={true} color="black" fontSize= "24px" label="STATE_LABEL" />
+           </div>
+          </div>
+          <Label style={{ marginBottom: "12px" }} className="text-center" bold={true} dark={true} fontSize={16} label="CORE_COMMON_LOGIN" />
+          <Field fieldKey="phone" field={fields.phone} handleFieldChange={handleFieldChange}  />
+          <div style={{ marginBottom: "24px", position: "relative", zIndex: 10 }} className="text-right">
+            <Label id="otp-trigger" className="otp-prompt" label="CORE_LOGIN_NO_ACCOUNT" />
+            <Link to="/user/register">
+              <div style={{ display: "inline-block" }}>
+                <Label containerStyle={{ cursor: "pointer" }} id="otp-resend" className="otp-resend" label="CORE_REGISTER_HEADING" />
+              </div>
+            </Link>
+          </div>
+          <Button
+            {...submit}
+            fullWidth={true}
+            primary={true}
+            onClick={(e) => {
+              startSMSRecevier();
+            }}
+          />
+          {enableWhatsApp&&
+           <Hidden mdUp>
+          <div>
+        <div className="login-hl-divider">
+       <div className ="login-circle-mobile">
+       <Label  color="black" fontSize= "16px" label="Or"/>
+       </div>
+    </div>
+    <div className="rainmaker-displayInline login-mobile-whatsapp-button"  onClick={()=>{window.location.href="https://api.whatsapp.com/send?phone=918744960111&text=mSeva-send+this+to+start"}} >      
+        <Icon action="custom" name="whatsapp" className="login-mobile-whatsapp-icon" />
+        <Label bold={true} color="black" fontSize= "14px" label="WHATSAPP_CONTINUE_MOBILE"/>
+    </div>
+    </div>
+      </Hidden>
+      }
+        </div>
+      }
+    />
+    </Hidden>
+}
     {enableWhatsApp&&
       <Hidden smDown>
+          <Card
+      className="wha-user-screen-card"
+      textChildren={
+        <div>
+          <div className="rainmaker-displayInline" style={{ justifyContent: "center" }}>
+            <div style={{ marginBottom: "24px" }}>
+              <Image className="mseva-logo" source={`${logo}`} />
+            </div >
+          <div style={{marginLeft:"7px", marginBottom: "24px" }}>
+          <Label bold={true}  fontSize= "23px" label="|" />
+          </div>
+           <div style={{marginLeft:"7px" }}>
+              <Label bold={true} color="black" fontSize= "24px" label="STATE_LABEL" />
+           </div>
+          </div>
+          <div className="rainmaker-displayInline" style={{ justifyContent: "center" }}>
+          <div style={{ width: "50%",marginTop:"4%"}}>
+          <Label style={{ marginBottom: "12px" }} className="text-center" bold={true} dark={true} fontSize={24} label="CORE_COMMON_LOGIN" />
+          <Field fieldKey="phone" field={fields.phone} handleFieldChange={handleFieldChange}  />
+          <div style={{ marginBottom: "24px", position: "relative", zIndex: 10 }} className="text-right">
+            <Label id="otp-trigger" className="otp-prompt" label="CORE_LOGIN_NO_ACCOUNT" />
+            <Link to="/user/register">
+              <div style={{ display: "inline-block" }}>
+                <Label containerStyle={{ cursor: "pointer" }} id="otp-resend" className="otp-resend" label="CORE_REGISTER_HEADING" />
+              </div>
+            </Link>
+          </div>
+          <Button
+            {...submit}
+            fullWidth={true}
+            primary={true}
+            onClick={(e) => {
+              startSMSRecevier();
+            }}
+          />
+   </div>
      <div className="login-vl-divider">
        <div className ="login-circle-web">
        <Label  color="black" fontSize= "16px" label="OR"/>
@@ -78,6 +172,10 @@ const LoginForm = ({ handleFieldChange, form, logoUrl,qrCodeURL,enableWhatsApp }
        <Label  color="black" fontSize= "14px" label="WHATSAPP_SCAN_QR_CODE"/>
        </div>
     </div>
+    </div>
+    </div>
+      }
+    />
     </Hidden>
 }
     </div>
