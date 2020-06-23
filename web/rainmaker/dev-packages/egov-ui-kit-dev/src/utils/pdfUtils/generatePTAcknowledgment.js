88f0495e4f40742081c1ee349a4dded2e5693fad
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2020-06-23 13:10:04 +0530

    Rain 1157 : TL :: Acknowledgement pdf alignment is not proper. (#1529)
    * RAIN-1157 : Font size increased
    
    * code clean up

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/pdfUtils/generatePTAcknowledgment.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/pdfUtils/generatePTAcknowledgment.js
@@ -3,7 +3,7 @@ import get from "lodash/get";
 import { getAssessmentInfo, getUnitInfo } from "../../common/propertyTax/Property/components/AssessmentInfo";
 import { getOwnerInfo } from "../../common/propertyTax/Property/components/OwnerInfo";
 import { getAddressItems } from "../../common/propertyTax/Property/components/PropertyAddressInfo";
-import { generatePDF, getDocumentsCard, getOwnerCard, getMultipleItemCard } from "./generatePDF";
+import { generatePDF, getDocumentsCard, getMultipleItemCard } from "./generatePDF";
 
 export const generatePTAcknowledgment = (property, generalMDMSDataById, UlbLogoForPdf, fileName = "acknowledgement.pdf") => {
 
@@ -48,7 +48,7 @@ export const generatePTAcknowledgment = (property, generalMDMSDataById, UlbLogoF
     property.owners = property.owners.filter(owner => owner.status == "ACTIVE")
     const ownerInfo = getOwnerInfo(property, generalMDMSDataById);
     const addressCard = getAddressItems(property);
-    const ownerCard = getMultipleItemCard(ownerInfo,'PT_OWNER');
+    const ownerCard = getMultipleItemCard(ownerInfo, 'PT_OWNER');
     const assessmentCard = getAssessmentInfo(get(property, 'propertyDetails[0]', {}), generalMDMSDataById);
     const documentCard = getDocumentsCard(property.documentsUploaded);
 
@@ -59,10 +59,9 @@ export const generatePTAcknowledgment = (property, generalMDMSDataById, UlbLogoF
         cards: [
             { header: "PT_PROPERTY_ADDRESS_SUB_HEADER", items: addressCard },
             { header: "PT_ASSESMENT_INFO_SUB_HEADER", items: assessmentCard },
-            { items: unitInfoCard, type: "multiItem" , hide: unitInfoCard.length===0},
+            { items: unitInfoCard, type: "multiItem", hide: unitInfoCard.length === 0 },
             { header: 'PT_OWNERSHIP_INFO_SUB_HEADER', items: ownerCard, type: ownerInfo.length > 1 ? 'multiItem' : 'singleItem' },
             { header: 'PT_COMMON_DOCS', items: documentCard }]
     }
-
     generatePDF(UlbLogoForPdf, pdfData, fileName);
 }
