b4e503a514ddd22f6c51fc7d1660d97bb9d480f9
karthik-egov-tarento <karthikeyan.rajendran@egovernments.com>
2020-07-09 04:47:51 +0530

    Typo fixed for attribute name

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/search-preview.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/search-preview.js
@@ -545,7 +545,7 @@ const searchResults = async (action, state, dispatch, applicationNumber,processI
     }]
     if (payload !== undefined && payload !== null) {
       dispatch(prepareFinalObject("WaterConnection[0]", payload.WaterConnection[0]));
-      if(!payload.SewerageConnections[0].connectionHolders || payload.SewerageConnections[0].connectionHolders === 'NA'){        
+      if(!payload.WaterConnection[0].connectionHolders || payload.WaterConnection[0].connectionHolders === 'NA'){        
         set(action.screenConfig, "components.div.children.taskDetails.children.cardContent.children.reviewConnectionDetails.children.cardContent.children.viewFive.visible",false);
       }else{
         set(action.screenConfig, "components.div.children.taskDetails.children.cardContent.children.reviewConnectionDetails.children.cardContent.children.viewSix.visible",false);
