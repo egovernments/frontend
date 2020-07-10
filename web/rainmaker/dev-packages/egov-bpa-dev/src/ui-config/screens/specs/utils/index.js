2ad9b848a3bb54c56100ad240761512c94da9849
udayalakshmi-wtt-egov <65272933+udayalakshmi-wtt-egov@users.noreply.github.com>
2020-07-10 19:30:02 +0530

    Changes related to #587 (#1623)

--- web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/screens/specs/utils/index.js
+++ web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/screens/specs/utils/index.js
@@ -4408,7 +4408,7 @@ if(requiredDocsFromMdms.length > 0){
     mdmsCard.documentCode = getTransformedLocale(mdmsCard.code);
     for(var i=0; i<cards.length; i++){
       if(mdmsCard.documentCode == cards[i].documentCode){
-        cards[i].readOnly = cardReadOnly;
+        cards[i].readOnly = cardReadOnly || !mdmsCard.allow;
         let mergedCard = {...cards[i], ...mdmsCard};
         cards[i] = {...mergedCard};
         found = true;
@@ -4416,7 +4416,7 @@ if(requiredDocsFromMdms.length > 0){
     }
     
     if(!found){
-      mdmsCard.readOnly = cardReadOnly;
+      mdmsCard.readOnly = cardReadOnly || !mdmsCard.allow;
       cards.push(mdmsCard)
     }
   });
@@ -4435,7 +4435,7 @@ cards.map(card=>{
 });
 dispatch(prepareFinalObject("finalCardsforPreview", cards));
 
-}
+ }
 /**
  * 
  * @param {String} documentType 
@@ -4513,6 +4513,7 @@ if (documents[0] && documents[0].length > 0) {
     card["name"] = doc.code;
     card["code"] = doc.code;
     card["required"] = doc.required ? true : false;
+    card["allow"] = (doc.allow && JSON.parse(doc.allow)) ? true: false;
     if (doc.hasDropdown && doc.dropDownValues) {
       let dropDownValues = {};
       dropDownValues.label = "Select Documents";
