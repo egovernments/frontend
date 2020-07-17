fbf2f0047cb7ffa6029e494bf6166b7da7d90ab4
vamshikrishnakole-wtt-egov <vamshikrishna.kole@egovernments.org>
2020-07-17 16:33:44 +0530

    added number

--- web/rainmaker/dev-packages/egov-bpa-dev/src/ui-utils/commons.js
+++ web/rainmaker/dev-packages/egov-bpa-dev/src/ui-utils/commons.js
@@ -675,7 +675,7 @@ const getNocDocuments = (state) =>{
         let url = `${window.location.origin}/egov-bpa/noc-search-preview?applicationNumber=${nocDoc.applicationNo}&tenantId=${nocDoc.tenantId}`;
         if (process.env.NODE_ENV === "production") {
           if (checkingApp) {
-            url = `${window.location.origin}/${checkingApp}/egov-bpa/search-preview?applicationNumber=${appNumber}&tenantId=${tenantId}`;
+            url = `${window.location.origin}/${checkingApp}/egov-bpa/search-preview?applicationNumber=${nocDoc.applicationNo}&tenantId=${tenantId}`;
           }
         }
         linkDetails.labelName = "Application Number"
