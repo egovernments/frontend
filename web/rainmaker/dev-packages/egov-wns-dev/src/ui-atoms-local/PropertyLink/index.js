b58a568a2efde376945765a205c7c90703d1ad0d
venkatesan-egov <64830241+venkatesan-egov@users.noreply.github.com>
2020-07-21 10:36:53 +0530

    WOR-376 - Implemented the Modify Property Edit Feature. (#1666)
    * WOR-375 - Implemented Add Meter Reading Restrict.
    
    * WOR-376 - Implemented the Modify Property Edit .
    
    Co-authored-by: venkat <venkatesan.r@goodworklabs.com>

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-atoms-local/PropertyLink/index.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-atoms-local/PropertyLink/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { LabelContainer } from "egov-ui-framework/ui-containers";
 import { getDomainLink } from "../../ui-utils/commons";
+import { getQueryArg } from "egov-ui-framework/ui-utils/commons";
+import { connect } from "react-redux";
+import get from "lodash/get";
 const styles = {
   color: "rgba(0, 0, 0, 0.87)",
   marginLeft: "3%",
@@ -14,36 +17,48 @@ const clickHereStyles = {
   textDecoration: "none",
   color: "#FE7A51"
 }
-
-function AddLinkForProperty(props) {
-  const { url,isMode } = props;
-  let link = window.location.origin;  
-  link += `${getDomainLink()}/pt-common-screens/propertySearch?redirectUrl=${url}`;
-  if(isMode==="MODIFY"){
-    return (
-      <div style={styles}>
-        <a href={`${link}`} ><LabelContainer
-            style={clickHereStyles}
-            labelKey="WS_SEARCH_PROPERTY" />
-        </a> <span> </span>
-        <LabelContainer
-            style={clickHereStyles}
-            labelKey="WS_MODIFY_PROPERTY" />
-      </div>
-    );
-  }else{
-    return (
-      <div style={styles}>
-        <LabelContainer
-          labelName="To Find/Create Property ID"
-          labelKey="WS_APPLY_CREATE_MSG" /><span> </span><a href={`${link}`} ><LabelContainer
-            style={clickHereStyles}
-            labelKey="WS_APPLY_CLICK_HERE" />
-        </a>
-      </div>
-    );
+class AddLinkForProperty extends React.Component {
+  render() {
+    const { url,isMode, selectedPropertyId } = this.props;
+    let link = window.location.origin;  
+    link += `${getDomainLink()}/pt-common-screens/propertySearch?redirectUrl=${url}`;
+    const tenantId = getQueryArg(window.location.href, "tenantId");
+    let modifyLink = window.location.origin + `/property-tax/assessment-form?purpose=update&propertyId=${selectedPropertyId}&tenantId=${ tenantId }&redirectTo=${url}`
+    if(isMode==="MODIFY"){
+      return (
+        <div style={styles}>
+          <a href={`${link}`} ><LabelContainer
+              style={clickHereStyles}
+              labelKey="WS_SEARCH_PROPERTY" />
+          </a> <span> </span>
+          <a href={`${modifyLink}`} >
+              <LabelContainer
+              style={clickHereStyles}
+              labelKey="WS_MODIFY_PROPERTY" />
+          </a>
+        </div>
+      );
+    } else {
+      return (
+        <div style={styles}>
+          <LabelContainer
+            labelName="To Find/Create Property ID"
+            labelKey="WS_APPLY_CREATE_MSG" /><span> </span><a href={`${link}`} ><LabelContainer
+              style={clickHereStyles}
+              labelKey="WS_APPLY_CLICK_HERE" />
+          </a>
+        </div>
+      );
+    }
   }
-  
 }
+const mapStateToProps = (state, ownprops) => {
+  let selectedPropertyId = "";
+  const { screenConfiguration } = state;
+  const { preparedFinalObject } = screenConfiguration;
+  selectedPropertyId = get(preparedFinalObject, "applyScreen.property.propertyId");
 
-export default AddLinkForProperty;
+  return { selectedPropertyId };
+};
+
+export default connect(mapStateToProps)(AddLinkForProperty);
