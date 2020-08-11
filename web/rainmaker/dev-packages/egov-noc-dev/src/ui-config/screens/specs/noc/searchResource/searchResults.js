072dc748329c8ae70a90c7ba7323c6ea5b26ad25
vamshikrishnakole-wtt-egov <57343517+vamshikrishnakole-wtt-egov@users.noreply.github.com>
2020-08-11 15:10:17 +0530

    NOC Module name changed (#1779)

--- web/rainmaker/dev-packages/egov-noc-dev/src/ui-config/screens/specs/noc/searchResource/searchResults.js
+++ web/rainmaker/dev-packages/egov-noc-dev/src/ui-config/screens/specs/noc/searchResource/searchResults.js
@@ -77,10 +77,10 @@ const onRowClick = rowData => {
   const origin = process.env.NODE_ENV === "production" ? window.location.origin + "/" : window.location.origin;
   switch (state) {
     case "INITIATED":
-      window.location.assign(`${origin}${environment}/egov-common-noc/search-preview?applicationNumber=${applicationNumber}&tenantId=${tenantId}`);
+      window.location.assign(`${origin}${environment}/noc/search-preview?applicationNumber=${applicationNumber}&tenantId=${tenantId}`);
       break;
     default:
-      window.location.assign(`${origin}${environment}/egov-common-noc/search-preview?applicationNumber=${applicationNumber}&tenantId=${tenantId}`);
+      window.location.assign(`${origin}${environment}/noc/search-preview?applicationNumber=${applicationNumber}&tenantId=${tenantId}`);
       break;
   }
 };
