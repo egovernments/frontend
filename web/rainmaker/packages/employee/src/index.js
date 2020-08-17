ccd4cb65f5041929ddac6fa94dad461f644e2213
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2020-08-17 15:18:29 +0530

    Pb build issue fix (#1810)
    * Update package.json
    
    * changed dependency version
    
    * changed dependency version
    
    * updated env
    
    * removed babel
    
    * Update package.json
    
    * remved babel
    
    * Update package.json
    
    * Update package.json
    
    * updated packages
    
    * Update package.json
    
    * removed dependencty for build issue
    
    * removed babel in citizen

--- web/rainmaker/packages/employee/src/index.js
+++ web/rainmaker/packages/employee/src/index.js
@@ -1,7 +1,6 @@
 // support for older browsers
-import "babel-polyfill";
 import "url-search-params-polyfill";
-import "./webview/web-share-shim.bundle.min";
+// import "./webview/web-share-shim.bundle.min";
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
