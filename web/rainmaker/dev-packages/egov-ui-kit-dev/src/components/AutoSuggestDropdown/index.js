8c366cd9d9793828d5bae6dd1bb4daabea243208
venkat <venkatesan.r@goodworklabs.com>
2020-08-21 17:23:59 +0530

    PI-583 : Added scroll for autosuggest dropdowns.

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/components/AutoSuggestDropdown/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/components/AutoSuggestDropdown/index.js
@@ -109,7 +109,7 @@ class AutoSuggestDropdown extends React.Component {
           searchText={searchText}
           underlineDisabledStyle={underlineDisabledStyle}
           dataSource={(items && [...items]) || []}
-          menuStyle={{ maxHeight: "150px", overflowY: "hidden" }}
+          menuStyle={{ maxHeight: "150px", overflowY: "scroll" }}
           dataSourceConfig={{ text: "label", value: "value" }}
           onNewRequest={onChange}
           onUpdateInput={onChangeText}
