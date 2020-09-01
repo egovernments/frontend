0afde70d5cf8a2a2e6922534252d7863a05790d0
Deependrakumarkumawat <deependra@egovernments.org>
2020-09-01 20:07:47 +0530

    msg open on citizen side

--- web/rainmaker/packages/citizen/src/modules/Routes/citizen.js
+++ web/rainmaker/packages/citizen/src/modules/Routes/citizen.js
@@ -4,127 +4,127 @@ import frameworkScreens from "./frameworkScreens";
 import pgrRoutes from "pgr-citizen/Routes/pgr-routes";
 import ptRoutes from "pt-citizen/Routes/pt-routes";
 
-const Loading = () => <div />;
+// const Loading = () => <div />;
 
 // user routes
-const Register = Loadable({
-  loader: () => import("modules/citizen/User/Register"),
-  loading: Loading,
-});
-// import Register from "modules/citizen/User/Register";
+// const Register = Loadable({
+//   loader: () => import("modules/citizen/User/Register"),
+//   loading: Loading,
+// });
+import Register from "modules/citizen/User/Register";
 
-const Login = Loadable({
-  loader: () => import("modules/citizen/User/Login"),
-  loading: Loading,
-});
-// import Login from "modules/citizen/User/Login";
+// const Login = Loadable({
+//   loader: () => import("modules/citizen/User/Login"),
+//   loading: Loading,
+// });
+import Login from "modules/citizen/User/Login";
 
-const OTP = Loadable({
-  loader: () => import("modules/citizen/User/OTP"),
-  loading: Loading,
-});
-// import OTP from "modules/citizen/User/OTP";
+// const OTP = Loadable({
+//   loader: () => import("modules/citizen/User/OTP"),
+//   loading: Loading,
+// });
+import OTP from "modules/citizen/User/OTP";
 
-const LanguageSelection = Loadable({
-  loader: () => import("modules/citizen/User/LanguageSelection"),
-  loading: Loading,
-});
-// import LanguageSelection from "modules/citizen/User/LanguageSelection";
+// const LanguageSelection = Loadable({
+//   loader: () => import("modules/citizen/User/LanguageSelection"),
+//   loading: Loading,
+// });
+import LanguageSelection from "modules/citizen/User/LanguageSelection";
 
-const Profile = Loadable({
-  loader: () => import("modules/citizen/User/Profile"),
-  loading: Loading,
-});
-// import Profile from "modules/citizen/User/Profile";
+// const Profile = Loadable({
+//   loader: () => import("modules/citizen/User/Profile"),
+//   loading: Loading,
+// });
+import Profile from "modules/citizen/User/Profile";
 
 // common screens
 
-//import LandingPage from "modules/citizen/LandingPage";
-const CitizenDashboard = Loadable({
-  loader: () => import("modules/citizen/CitizenDashboard"),
-  loading: Loading,
-});
-// import CitizenDashboard from "modules/citizen/CitizenDashboard";
+import LandingPage from "modules/citizen/LandingPage";
+// const CitizenDashboard = Loadable({
+//   loader: () => import("modules/citizen/CitizenDashboard"),
+//   loading: Loading,
+// });
+import CitizenDashboard from "modules/citizen/CitizenDashboard";
 
-const HowItWorks = Loadable({
-  loader: () => import("modules/citizen/HowItWorks"),
-  loading: Loading,
-});
-// import HowItWorks from "modules/citizen/HowItWorks";
+// const HowItWorks = Loadable({
+//   loader: () => import("modules/citizen/HowItWorks"),
+//   loading: Loading,
+// });
+import HowItWorks from "modules/citizen/HowItWorks";
 
-const ContactUs = Loadable({
-  loader: () => import("modules/citizen/ContactUs"),
-  loading: Loading,
-});
-// import ContactUs from "modules/citizen/ContactUs";
+// const ContactUs = Loadable({
+//   loader: () => import("modules/citizen/ContactUs"),
+//   loading: Loading,
+// });
+import ContactUs from "modules/citizen/ContactUs";
 
-const MyNotifications = Loadable({
-  loader: () => import("modules/citizen/MyNotifications"),
-  loading: Loading,
-});
-// import MyNotifications from "modules/citizen/MyNotifications";
+// const MyNotifications = Loadable({
+//   loader: () => import("modules/citizen/MyNotifications"),
+//   loading: Loading,
+// });
+import MyNotifications from "modules/citizen/MyNotifications";
 
-const WhatsNew = Loadable({
-  loader: () => import("modules/citizen/WhatsNew"),
-  loading: Loading,
-});
-// import WhatsNew from "modules/citizen/WhatsNew";
+// const WhatsNew = Loadable({
+//   loader: () => import("modules/citizen/WhatsNew"),
+//   loading: Loading,
+// });
+import WhatsNew from "modules/citizen/WhatsNew";
 
-const { EventDetails } = Loadable({
-  loader: () => import("modules/common"),
-  loading: Loading,
-});
-// import { EventDetails } from "modules/common";
+// const { EventDetails } = Loadable({
+//   loader: () => import("modules/common"),
+//   loading: Loading,
+// });
+import { EventDetails } from "modules/common";
 
-const Events = Loadable({
-  loader: () => import("modules/citizen/Events"),
-  loading: Loading,
-});
-// import Events from "modules/citizen/Events";
+// const Events = Loadable({
+//   loader: () => import("modules/citizen/Events"),
+//   loading: Loading,
+// });
+import Events from "modules/citizen/Events";
 
-const MyCity = Loadable({
-  loader: () => import("modules/citizen/MyCity"),
-  loading: Loading,
-});
-// import MyCity from "modules/citizen/MyCity";
+// const MyCity = Loadable({
+//   loader: () => import("modules/citizen/MyCity"),
+//   loading: Loading,
+// });
+import MyCity from "modules/citizen/MyCity";
 
-const { ImageModalDisplay } = Loadable({
-  loader: () => import("modules/common"),
-  loading: Loading,
-});
-// import { ImageModalDisplay } from "modules/common";
+// const { ImageModalDisplay } = Loadable({
+//   loader: () => import("modules/common"),
+//   loading: Loading,
+// });
+import { ImageModalDisplay } from "modules/common";
 
 // pgr specific screens
-const { TrackLocation } = Loadable({
-  loader: () => import("modules/common"),
-  loading: Loading,
-});
-// import { TrackLocation } from "modules/common";
+// const { TrackLocation } = Loadable({
+//   loader: () => import("modules/common"),
+//   loading: Loading,
+// });
+import { TrackLocation } from "modules/common";
 
-const { PrivacyPolicy } = Loadable({
-  loader: () => import("modules/common"),
-  loading: Loading,
-});
-// import { PrivacyPolicy } from "modules/common";
+// const { PrivacyPolicy } = Loadable({
+//   loader: () => import("modules/common"),
+//   loading: Loading,
+// });
+import { PrivacyPolicy } from "modules/common";
 
-const TLHowItWorks = Loadable({
-  loader: () => import("../../ui-views/HowItWorks"),
-  loading: Loading,
-});
-// import TLHowItWorks from "../../ui-views/HowItWorks";
+// const TLHowItWorks = Loadable({
+//   loader: () => import("../../ui-views/HowItWorks"),
+//   loading: Loading,
+// });
+import TLHowItWorks from "../../ui-views/HowItWorks";
 
 //Whatsapp Screen
-const WhatsAppCity = Loadable({
-  loader: () => import("modules/citizen/WhatsAppScreen/City"),
-  loading: Loading,
-});
-// import  WhatsAppCity from "modules/citizen/WhatsAppScreen/City";
+// const WhatsAppCity = Loadable({
+//   loader: () => import("modules/citizen/WhatsAppScreen/City"),
+//   loading: Loading,
+// });
+import  WhatsAppCity from "modules/citizen/WhatsAppScreen/City";
 
-const WhatsAppLocality = Loadable({
-  loader: () => import("modules/citizen/WhatsAppScreen/Locality"),
-  loading: Loading,
-});
-// import  WhatsAppLocality from "modules/citizen/WhatsAppScreen/Locality";
+// const WhatsAppLocality = Loadable({
+//   loader: () => import("modules/citizen/WhatsAppScreen/Locality"),
+//   loading: Loading,
+// });
+import  WhatsAppLocality from "modules/citizen/WhatsAppScreen/Locality";
 
 const routes = [
   {
