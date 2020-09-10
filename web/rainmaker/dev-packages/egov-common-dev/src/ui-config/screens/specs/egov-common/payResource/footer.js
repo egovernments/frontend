7acd238901dc482f1d10107d3a290a6f114df417
jayachandra-wt-egov <jayachandra.choppa@egovernments.org>
2020-09-10 15:46:06 +0530

    payer name and paid by chnages for common pay

--- web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/payResource/footer.js
+++ web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/payResource/footer.js
@@ -361,7 +361,7 @@ const callBackForPay = async (state, dispatch) => {
 
   ReceiptBodyNew.Payment["paymentMode"] =
     finalReceiptData.instrument.instrumentType.name;
-  ReceiptBodyNew.Payment["paidBy"] = finalReceiptData.Bill[0].payer;
+  ReceiptBodyNew.Payment["paidBy"] = finalReceiptData.Bill[0].paidBy;
   ReceiptBodyNew.Payment["mobileNumber"] =
     finalReceiptData.Bill[0].payerMobileNumber;
     ReceiptBodyNew.Payment["payerName"] = finalReceiptData.Bill[0].paidBy?finalReceiptData.Bill[0].paidBy:(finalReceiptData.Bill[0].payerName||finalReceiptData.Bill[0].payer);
