60e51f783910cee984cd65fe60daca814c1759a0
ragini <pb.ragini@gmail.com>
2020-09-25 11:19:30 +0530

    changed tooltip font size to check in citizen

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/components/Tooltip/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/components/Tooltip/index.js
@@ -21,7 +21,7 @@ const PopperProps = {
 
 const ToolTipUi = ({ placement, show, title, id }) => {
   return (
-    <Tooltip id={id} title={<Label label={title} color="#fff" fontSize="12px" />} placement={placement || "right"} PopperProps={PopperProps}>
+    <Tooltip id={id} title={<Label label={title} color="#fff" fontSize="16px" />} placement={placement || "right"} PopperProps={PopperProps}>
       <Icon color="disabled" style={{ fontSize: 24 }}>
         <InfoIcon />
       </Icon>
