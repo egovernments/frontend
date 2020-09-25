1bb4415ce93a6af8405354c4a31d11ddff9bef7b
egovmis <42794267+egovmis@users.noreply.github.com>
2020-09-25 17:18:39 +0530

    changed PT application PDF PI-8231

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/AcknowledgementReceipt/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/AcknowledgementReceipt/index.js
@@ -15,11 +15,11 @@ pdfMake.fonts = {
 };
 
 export const AcknowledgementReceipt = (role, details, generalMDMSDataById, receiptImageUrl, isEmployeeReceipt) => {
-  console.log("details--" + details);
-  console.log(generalMDMSDataById);
+  console.log("details--" , details);
+  //console.log(generalMDMSDataById);
   let data;
   let { owners, address, propertyDetails, header, propertyId } = details;
-  let dateArray = new Date(propertyDetails[0].assessmentDate).toDateString().split(" ");
+  let dateArray = new Date().toDateString().split(" ");
   let assessmentDate = dateArray[2] + "-" + dateArray[1] + "-" + dateArray[3];
   let tableborder = {
     hLineColor: function(i, node) {
@@ -75,7 +75,7 @@ export const AcknowledgementReceipt = (role, details, generalMDMSDataById, recei
           dataRow.push({ text: getLocaleLabels("Sub Usage Type", "PT_ACK_LOCALIZATION_SUB_USAGE_TYPE"), style: "receipt-assess-table-header" });
           dataRow.push({ text: getLocaleLabels("Occupancy", "PT_ACK_LOCALIZATION_OCCUPANCY"), style: "receipt-assess-table-header" });
           dataRow.push({
-            text: getLocaleLabels("Built Area/Total Annual Rent(sq yards)", "PT_ACK_LOCALIZATION_BUILTAREA_TOTAL_RENT"),
+            text: "Built up Area(sq feet)/Total Annual Rent",
             style: "receipt-assess-table-header",
           });
           bodyData.push(dataRow);
@@ -100,6 +100,7 @@ export const AcknowledgementReceipt = (role, details, generalMDMSDataById, recei
                 )
               );
               dataRow.push(
+                unit.usageCategoryDetail === false?"NA":(
                 getLocaleLabels(
                   "PROPERTYTAX_BILLING_SLAB_" +
                     transform(
@@ -111,7 +112,7 @@ export const AcknowledgementReceipt = (role, details, generalMDMSDataById, recei
                       unit.usageCategoryDetail ? unit.usageCategoryDetail : unit.usageCategorySubMinor,
                       unit.usageCategoryDetail ? "UsageCategoryDetail" : "UsageCategorySubMinor"
                     )
-                )
+                ))
               );
               dataRow.push(
                 getLocaleLabels(
@@ -122,7 +123,7 @@ export const AcknowledgementReceipt = (role, details, generalMDMSDataById, recei
               if (unit.occupancyType === "RENTED") {
                 dataRow.push(unit.arv || "");
               } else {
-                dataRow.push(`${Math.round(unit.unitArea * 100) / 100}` || "");
+                dataRow.push(`${Math.round(unit.unitArea * 9)}` || "");
               }
 
               bodyData.push(dataRow);
@@ -302,97 +303,45 @@ export const AcknowledgementReceipt = (role, details, generalMDMSDataById, recei
               },
             ],
           },
-          { text: "PROPERTY ADDRESS", style: "pt-reciept-citizen-subheader" },
           {
             style: "receipt-header-details",
             columns: [
               {
-                stack:[
-                    {
-                        text: "City: ",
-                        bold: true,
-                    },
-                    {
-                        text: address.city || ""
-                    }
-                ],
-                width: 140,
-                margin:[0,10,0,10]
-              },
-              {
-                stack: [
+                text: [
                   {
-                    text: "Door/House No. ",
+                    text: "Application No.",
                     bold: true,
                   },
-                  {text: address.doorNo || "",}
+                  details.applicationNo || "NA",
                 ],
-                width: 140,
-                margin:[0,10,0,10]
+                alignment: "left",
               },
+            //   {
+            //     text: [
+            //       {
+            //         text: "Assessment No.",
+            //         bold: true,
+            //       },
+            //       "PT-AS-2020-02-13-018679" || getLocaleLabels("PT_LOCALIZATION_NOT_AVAILABLE", "PT_LOCALIZATION_NOT_AVAILABLE"),
+            //     ],
+            //     alignment: "center",
+            //   },
               {
-                stack: [
+                text: [
                   {
-                    text: "Building/Company Name: ",
+                    text: "",
                     bold: true,
                   },
-                  {text: address.buildingName || "Shobha apartments"},
+                "",
+                  // receipts.paymentDate || "",  //todo
                 ],
-                width: 140,
-                margin:[0,10,0,10]
-              },
-              {
-                stack: [
-                  {
-                    text: "Street Name: ",
-                    bold: true,
-                  },
-                  {text: address.street || "3rd block"},
-                ],
-                width: 140,
-                margin:[0,10,0,10]
-              },
-              
-            ],
-          },
-          {
-            style: "receipt-header-details",
-            columns: [
-              {
-                stack: [
-                  {
-                    text: "Mohalla: ",
-                    bold: true,
-                  },
-                  {text: address.locality.name || "Ajit Nagar",}
-                ],
-                width: 140,
-                margin:[0,10,0,10]
-              },
-              {
-                stack: [
-                  {
-                    text: "Pincode: ",
-                    bold: true,
-                  },
-                  {text: address.pincode || "123456"},
-                ],
-                width: 140,
-                margin:[0,10,0,10]
-              },
-              {
-                stack: [
-                  {
-                    text: "Existing Proprty ID: ",
-                    bold: true,
-                  },
-                  {text: details.oldPropertyId || "PT-107-023456"},
-                ],
-                width: 140,
-                margin:[0,10,0,10]
+
+                alignment: "right",
               },
             ],
           },
+     
+     
           //   {
           //     style: "pt-reciept-citizen-table",
           //     table: {
@@ -411,82 +360,113 @@ export const AcknowledgementReceipt = (role, details, generalMDMSDataById, recei
           //     layout: tableborder,
           //   },
           { text: getLocaleLabels("PROPERTY ADDRESS", "PT_ACK_LOCALIZATION_PROPERTY_ADDRESS"), style: "pt-reciept-citizen-subheader" },
-          //   {
-          //     style: "pt-reciept-citizen-table",
-          //     table: {
-          //       widths: ['25%','25%','25%','25%'],
-          //       body: [
-          //         [
-          //           { text: getLocaleLabels("House/Door No.:","PT_ACK_LOCALIZATION_HOUSE_DOOR_NO"), border: borderKey, style: "receipt-table-key" },
-          //           { text: address.doorNo || getLocaleLabels("PT_LOCALIZATION_NOT_AVAILABLE","PT_LOCALIZATION_NOT_AVAILABLE"), border: borderValue },
-          //           { text: getLocaleLabels("Building/Colony Name.:","PT_ACK_LOCALIZATION_BUILDING_COLONY_NAME"), border: borderKey, style: "receipt-table-key" },
-          //           { text: address.buildingName || getLocaleLabels("PT_LOCALIZATION_NOT_AVAILABLE","PT_LOCALIZATION_NOT_AVAILABLE"), border: borderValue },
-          //         ],
-          //         [
-          //           { text: getLocaleLabels("Street Name:","PT_ACK_LOCALIZATION_STREET_NAME"), border: borderKey, style: "receipt-table-key" },
-          //           { text: address.street || getLocaleLabels("PT_LOCALIZATION_NOT_AVAILABLE","PT_LOCALIZATION_NOT_AVAILABLE"), border: borderValue },
-          //           { text: getLocaleLabels("Locality/Mohalla:","PT_ACK_LOCALIZATION_LOCALITY_MOHALLA"), border: borderKey, style: "receipt-table-key" },
-          //           { text: getLocaleLabels((address.tenantId.replace('.','_')+'_REVENUE_'+address.locality.code).toUpperCase(),(address.tenantId.replace('.','_')+'_REVENUE_'+address.locality.code).toUpperCase()),
-          //           border: borderValue
-          //          },
+             {
+               style: "pt-reciept-citizen-table",
+               table: {
+                 widths: ['25%','25%','25%','25%'],
+                body: [
+                   [
+                     { text: getLocaleLabels("House/Door No.:","PT_ACK_LOCALIZATION_HOUSE_DOOR_NO"), border: borderKey, style: "receipt-table-key" },
+                     { text: address.doorNo || getLocaleLabels("PT_LOCALIZATION_NOT_AVAILABLE","PT_LOCALIZATION_NOT_AVAILABLE"), border: borderValue },
+                     { text: getLocaleLabels("Building/Colony Name.:","PT_ACK_LOCALIZATION_BUILDING_COLONY_NAME"), border: borderKey, style: "receipt-table-key" },
+                     { text: address.buildingName || getLocaleLabels("PT_LOCALIZATION_NOT_AVAILABLE","PT_LOCALIZATION_NOT_AVAILABLE"), border: borderValue },
+                   ],
+                   [
+                     { text: getLocaleLabels("Street Name:","PT_ACK_LOCALIZATION_STREET_NAME"), border: borderKey, style: "receipt-table-key" },
+                     { text: address.street || getLocaleLabels("PT_LOCALIZATION_NOT_AVAILABLE","PT_LOCALIZATION_NOT_AVAILABLE"), border: borderValue },
+                     { text: getLocaleLabels("Locality/Mohalla:","PT_ACK_LOCALIZATION_LOCALITY_MOHALLA"), border: borderKey, style: "receipt-table-key" },
+                     { text: address.locality.name, border: borderValue },
+                   ],
+                   [
+                    { text: "Pin Code", border: borderKey, style: "receipt-table-key" },
+                    { text: address.pincode || getLocaleLabels("PT_LOCALIZATION_NOT_AVAILABLE","PT_LOCALIZATION_NOT_AVAILABLE"), border: borderValue },
+                    { text: "Existing Property ID:",border: borderKey, style: "receipt-table-key" },
+                    { text: details.oldPropertyId || "NA", border: borderValue },
+                  ],
+                 ],
+               },
+               layout: tableborder,
+             },
+             { text: getLocaleLabels("ASSESSMENT INFORMATION","PT_ACK_LOCALIZATION_ASSESSMENT_INFORMATION"), style: "pt-reciept-citizen-subheader" },
+             {
+               style: "pt-reciept-citizen-table",
+               table: {
+                widths: ['25%','25%','25%','25%'],
+                 body: [
+                   [
+                     { text: getLocaleLabels("Plot Size(sq yards)","PT_ACK_LOCALIZATION_PLOTSIZE_SQ_YARDS"), border: borderKey, style: "receipt-table-key" },
+                     { text: propertyDetails[0].landArea ? `${Math.round(propertyDetails[0].landArea * 100) / 100}` : (propertyDetails[0].buildUpArea ? `${Math.round(propertyDetails[0].buildUpArea * 100) / 100}` : "NA"), border: borderValue },
+                     { text: getLocaleLabels("Property Type:","PT_ACK_LOCALIZATION_PROPERTY_TYPE"),  border: borderKey, style: "receipt-table-key" },
+                     {
+                       text: propertyDetails[0].propertySubType
+                         ? getLocaleLabels("PROPERTYTAX_BILLING_SLAB_"+transform(propertyDetails[0].propertySubType, "PropertySubType"),"PROPERTYTAX_BILLING_SLAB_"+transform(propertyDetails[0].propertySubType, "PropertySubType"))
 
-          //         ],
-          //       ],
-          //     },
-          //     layout: tableborder,
-          //   },
-          //   { text: getLocaleLabels("ASSESSMENT INFORMATION","PT_ACK_LOCALIZATION_ASSESSMENT_INFORMATION"), style: "pt-reciept-citizen-subheader" },
-          //   {
-          //     style: "pt-reciept-citizen-table",
-          //     table: {
-          //       widths: ['25%','25%','25%','25%'],
-          //       body: [
-          //         [
-          //           { text: getLocaleLabels("Plot Size(sq yards)","PT_ACK_LOCALIZATION_PLOTSIZE_SQ_YARDS"), border: borderKey, style: "receipt-table-key" },
-          //           { text: propertyDetails[0].landArea ? `${Math.round(propertyDetails[0].landArea * 100) / 100}` : (propertyDetails[0].buildUpArea ? `${Math.round(propertyDetails[0].buildUpArea * 100) / 100}` : "NA"), border: borderValue },
-          //           { text: getLocaleLabels("Property Type:","PT_ACK_LOCALIZATION_PROPERTY_TYPE"),  border: borderKey, style: "receipt-table-key" },
-          //           {
-          //             text: propertyDetails[0].propertySubType
-          //               ? getLocaleLabels("PROPERTYTAX_BILLING_SLAB_"+transform(propertyDetails[0].propertySubType, "PropertySubType"),"PROPERTYTAX_BILLING_SLAB_"+transform(propertyDetails[0].propertySubType, "PropertySubType"))
+                         : getLocaleLabels("PROPERTYTAX_BILLING_SLAB_"+transform(propertyDetails[0].propertyType, "PropertyType"),"PROPERTYTAX_BILLING_SLAB_"+transform(propertyDetails[0].propertyType, "PropertyType")),
+                       border: borderValue,
+                     },
+                   ],
+                 ],
+               },
+               layout: tableborder,
+             },
+        floorData && {
+             text: getLocaleLabels("BUILT-UP AREA DETAILS", "PT_ACK_LOCALIZATION_BUILT_AREA_DETAILS"),
+             style: "pt-reciept-citizen-subheader",
+           },
+           floorData && {
+            style: "receipt-assess-table",
+             table: {
+              widths: ["20%", "20%", "20%", "20%", "20%"],
+              body: floorData,
+             },
+             layout: tableborder,
+           },
+           { text: getLocaleLabels("OWNERSHIP INFORMATION", "PT_ACK_LOCALIZATION_OWNERSHIP_INFORMATION"), style: "pt-reciept-citizen-subheader" },
+           {
+             style: "pt-reciept-citizen-table",
+             table: {
+               widths: ["25%", "25%", "25%", "25%"],
+               body: getOwnerDetails(owners, 4),
+             },
+             layout: tableborder,
+           },
+           { text: getLocaleLabels("Additional Details", "PT_ACK_LOCALIZATION_PROPERTY_ADDRESS"), style: "pt-reciept-citizen-subheader" },
+           {
+             style: "pt-reciept-citizen-table",
+             table: {
+               widths: ['25%','25%','25%','25%'],
+              body: [
+                 [
+                   { text: "Vasika No.:", border: borderKey, style: "receipt-table-key" },
+                   { text: propertyDetails[0].additionalDetails.vasikaNo|| "NA", border: borderValue },
+                   { text: "Vasika Date:", border: borderKey, style: "receipt-table-key" },
+                   { text: propertyDetails[0].additionalDetails.vasikaDate|| "NA", border: borderValue },
+                 ],
+                 [
+                  { text: "Allotment Letter No.:", border: borderKey, style: "receipt-table-key" },
+                  { text: propertyDetails[0].additionalDetails.allotmentNo|| "NA", border: borderValue },
+                  { text: "Allotment Letter Date:", border: borderKey, style: "receipt-table-key" },
+                  { text: propertyDetails[0].additionalDetails.allotmentDate|| "NA", border: borderValue },
+                ],
+                 [
+                   { text: "Firm/Business Name:", border: borderKey, style: "receipt-table-key" },
+                   { text: propertyDetails[0].additionalDetails.businessName || "NA", border: borderValue },
+                   { text: "Remarks", border: borderKey, style: "receipt-table-key" },
+                   { text: propertyDetails[0].additionalDetails.remrks || "NA", border: borderValue },
+                 ],
+               ],
+             },
+             layout: tableborder,
+           },
 
-          //               : getLocaleLabels("PROPERTYTAX_BILLING_SLAB_"+transform(propertyDetails[0].propertyType, "PropertyType"),"PROPERTYTAX_BILLING_SLAB_"+transform(propertyDetails[0].propertyType, "PropertyType")),
-          //             border: borderValue,
-          //           },
-          //         ],
-          //       ],
-          //     },
-          //     layout: tableborder,
-          //   },
-        //   floorData && {
-        //     text: getLocaleLabels("BUILT-UP AREA DETAILS", "PT_ACK_LOCALIZATION_BUILT_AREA_DETAILS"),
-        //     style: "pt-reciept-citizen-subheader",
-        //   },
-        //   floorData && {
-        //     style: "receipt-assess-table",
-        //     table: {
-        //       widths: ["20%", "20%", "20%", "20%", "20%"],
-        //       body: floorData,
-        //     },
-        //     layout: tableborder,
-        //   },
-        //   { text: getLocaleLabels("OWNERSHIP INFORMATION", "PT_ACK_LOCALIZATION_OWNERSHIP_INFORMATION"), style: "pt-reciept-citizen-subheader" },
-        //   {
-        //     style: "pt-reciept-citizen-table",
-        //     table: {
-        //       widths: ["25%", "25%", "25%", "25%"],
-        //       body: getOwnerDetails(owners, 4),
-        //     },
-        //     layout: tableborder,
-        //   },
-
-        //   {
-        //     text: getLocaleLabels("Commissioner/EO", "PT_ACK_LOCALIZATION_COMMISSIONER_EO"),
-        //     alignment: "right",
-        //     color: "#484848",
-        //     fontSize: 12,
-        //     bold: true,
-        //     margin: [0, 30, 0, 30],
-        //   },
+           {
+             text: getLocaleLabels("Commissioner/EO", "PT_ACK_LOCALIZATION_COMMISSIONER_EO"),
+             alignment: "right",
+             color: "#484848",
+             fontSize: 12,
+             bold: true,
+             margin: [0, 30, 0, 30],
+           },
         ],
 
         styles: {
@@ -550,5 +530,5 @@ export const AcknowledgementReceipt = (role, details, generalMDMSDataById, recei
     default:
   }
   // data && pdfMake.createPdf(data).download(`${propertyDetails[0].assessmentNumber}.pdf`);
-  data && pdfMake.createPdf(data).open();
+  data && pdfMake.createPdf(data).download("preview-"+details.propertyId+".pdf");
 };
