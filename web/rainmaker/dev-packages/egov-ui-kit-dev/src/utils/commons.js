343d9f5999da2e4d2e21236f5c954508d373e610
minju <minju.sarah@gmail.com>
2020-09-29 15:26:09 +0530

    changed commpay module name

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/commons.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/commons.js
@@ -1039,7 +1039,7 @@ export const getModuleName = () => {
   else if (pathName.indexOf("fire-noc") > -1) { return "rainmaker-noc,rainmaker-pgr"; }
   else if (pathName.indexOf("language-selection") > -1) { return "rainmaker-common"; }
   else if (pathName.indexOf("login") > -1) { return "rainmaker-common"; }
-  else if (pathName.indexOf("pay") > -1) { return "rainmaker-noc"; }
+  else if (pathName.indexOf("pay") > -1) { return "rainmaker-commonpay"; }
   else if (pathName.indexOf("abg") > -1) { return "rainmaker-abg"; }
   else if (pathName.indexOf("uc") > -1) { return "rainmaker-uc"; }
   else if (pathName.indexOf("pgr-home") > -1 || pathName.indexOf("rainmaker-pgr") > -1) { return "rainmaker-pgr"; }
