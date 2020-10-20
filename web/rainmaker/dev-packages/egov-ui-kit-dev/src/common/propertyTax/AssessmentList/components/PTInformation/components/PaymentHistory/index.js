f5119537ba387a4359d3d39fd4d67254853500b3
Jagankumar-Egov <jagan.kumar@egovernments.org>
2020-10-20 13:41:52 +0530

    RAIN-1690 : PT Added a Receipt status Field in Payment History section

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/AssessmentList/components/PTInformation/components/PaymentHistory/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/AssessmentList/components/PTInformation/components/PaymentHistory/index.js
@@ -48,6 +48,7 @@ class PaymentHistory extends Component {
                 <div>
                     {getFullRow("PT_HISTORY_RECEIPT_NO", payment.paymentDetails[0].receiptNumber ? '' + payment.paymentDetails[0].receiptNumber : "NA", 12)}
                     {getFullRow("PT_HISTORY_AMOUNT_PAID", amount ? 'Rs ' + amount : "NA", 12)}
+                    {getFullRow("PT_HISTORY_PAYMENT_STATUS", payment.paymentStatus ? payment.paymentStatus  : "NA", 12)}
                     {getFullRow("PT_HISTORY_PAYMENT_DATE", payment.transactionDate ? getFormattedDate(payment.transactionDate) : "NA", 12)}
                     {getFullRow("PT_HISTORY_BILL_NO", payment.paymentDetails[0].bill.billNumber ? '' + payment.paymentDetails[0].bill.billNumber : "NA", 12)}
                     {getFullRow("PT_HISTORY_BILL_PERIOD", this.getBillPeriod(payment.paymentDetails[0].bill.billDetails), 6)}
