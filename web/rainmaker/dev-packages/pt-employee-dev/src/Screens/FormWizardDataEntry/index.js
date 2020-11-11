aca3294d5846500318dec687706c9d7e742ad40d
guruprasadreddy-wt-egov <guruprasadreddy.narala@egovernments.org>
2020-11-11 11:10:33 +0530

    UKI - 834 Unable to update migrated property (Fixed) bcoz rebate zero

--- web/rainmaker/dev-packages/pt-employee-dev/src/Screens/FormWizardDataEntry/index.js
+++ web/rainmaker/dev-packages/pt-employee-dev/src/Screens/FormWizardDataEntry/index.js
@@ -1467,6 +1467,12 @@ class FormWizardDataEntry extends Component {
                     if (key2 == 0) {
                       previousKey = key;
                     }
+                    if (data.demand[data1][data2].PT_TAXHEAD === "PT_TIME_REBATE"|| data.demand[data1][data2].PT_TAXHEAD === "PT_PROMOTIONAL_REBATE" && data.demand[data1][data2].PT_DEMAND ==='0') 
+                    {  
+                    hasPropertyTax = true;  
+                      data.demand[data1][data2].PT_DEMAND = -0;                 
+
+                    }
 
                     // }
                   }
