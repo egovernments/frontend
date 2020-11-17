53fd1f077dc062f553333ed67be7028a89274c54
guruprasadreddy-wt-egov <guruprasadreddy.narala@egovernments.org>
2020-11-17 10:22:30 +0530

    UKI - 844 Common Page Changes

--- web/egov-common-screen/src/components/LandingPage/index.js
+++ web/egov-common-screen/src/components/LandingPage/index.js
@@ -43,6 +43,7 @@ class LandingPage extends React.Component {
   state = {
     backgroundUrl: "",
     logoUrl: "",
+    stateLogoUrl :"",
   };
 
   componentDidMount() {
@@ -92,6 +93,11 @@ class LandingPage extends React.Component {
           "logoUrl",
           response.data.MdmsRes["common-masters"].StateInfo[0].logoUrl
         );
+
+        this.setValuestoState(
+          "stateLogoUrl",
+          response.data.MdmsRes["common-masters"].StateInfo[0].stateLogoUrl
+        );        
       })
       .catch((err) => {
         console.log(err, "error");
@@ -104,7 +110,7 @@ class LandingPage extends React.Component {
   };
   render() {
     const { classes } = this.props;
-    const { backgroundUrl, logoUrl } = this.state;
+    const { backgroundUrl, logoUrl, stateLogoUrl} = this.state;
 
     console.log("state", this.state);
 
@@ -118,11 +124,10 @@ class LandingPage extends React.Component {
       >
         <Grid container className={classes.root}>
           <Grid container style={{ marginBottom: "10px" }}>
-            <Grid
+          <Grid
               item
               md={12}
               style={{
-                textAlign: "center",
                 backgroundColor: "#FFF",
                 paddingTop: "20px",
                 height: "80px",
@@ -131,9 +136,17 @@ class LandingPage extends React.Component {
             >
               <img
                 className="header-logo"
+                src={stateLogoUrl}
+                alt="company-logo"
+                width="20%"
+                style={{ float: 'left' }}
+              />
+              <img
+                className="header-logo"
                 src={logoUrl}
                 alt="company-logo"
                 width="20%"
+                style={{ float: 'right' }}
               />
             </Grid>
           </Grid>
