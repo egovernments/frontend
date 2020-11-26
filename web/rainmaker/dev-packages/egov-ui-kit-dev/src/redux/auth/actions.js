0bc57ac601a02a2fc536bece7e9b1d94155e9d93
ragini <pb.ragini@gmail.com>
2020-11-26 12:07:42 +0530

    access token in logout sent with request body instead of url

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/redux/auth/actions.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/redux/auth/actions.js
@@ -133,9 +133,24 @@ export const logout = () => {
   return async () => {
     try {
       const authToken = getAccessToken();
+
       if (authToken) {
-        const response = await httpRequest(AUTH.LOGOUT.URL, AUTH.LOGOUT.ACTION, [{ key: "access_token", value: authToken }]);
-      } else {
+        let requestBody =
+          { "access_token": authToken }
+
+          ;
+        console.log("requestBody", requestBody)
+        const response = await httpRequest(
+          AUTH.LOGOUT.URL,
+          AUTH.LOGOUT.ACTION,
+          [],
+          requestBody,
+
+        );
+        // const response = await httpRequest(AUTH.LOGOUT.URL, AUTH.LOGOUT.ACTION, [{ key: "access_token", value: authToken }]);
+      }
+      //const response = await httpRequest(AUTH.LOGOUT.URL, AUTH.LOGOUT.ACTION, []); 
+      else {
         clearUserDetails();
         process.env.REACT_APP_NAME === "Citizen"
           ? window.location.replace(`${window.basename}/user/register`)
