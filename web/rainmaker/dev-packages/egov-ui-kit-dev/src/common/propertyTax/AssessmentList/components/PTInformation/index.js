9512644b4f8aa5397defe0c3286992cf32fef42a
egovmis <mis.pmidc@punjab.gov.in>
2020-12-07 15:37:12 +0530

    modified ulb grade as per tenant id on PT application

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/AssessmentList/components/PTInformation/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/AssessmentList/components/PTInformation/index.js
@@ -72,8 +72,8 @@ class PTInformation extends React.Component {
      logoUrl = window.location.origin + `/pb-egov-assets/${tenantid}/logo.png`;
       corpCity = `TENANT_TENANTS_${get(properties, "tenantId").toUpperCase().replace(/[.:-\s\/]/g, "_")}`;
       const selectedCityObject = cities && cities.length > 0 && cities.filter(item => item.code === get(properties, "tenantId"));
-      // ulbGrade = selectedCityObject ? `ULBGRADE_${get(selectedCityObject[0], "city.ulbGrade")}` : "MUNICIPAL CORPORATION";
-      ulbGrade= "MUNICIPAL CORPORATION";
+      ulbGrade = selectedCityObject ? get(selectedCityObject[0], "city.ulbType").toUpperCase() : "MUNICIPAL CORPORATION";
+      //ulbGrade= "MUNICIPAL CORPORATION";
     }
     let propertyExist = properties.hasOwnProperty('propertyDetails');
     if(propertyExist) {
