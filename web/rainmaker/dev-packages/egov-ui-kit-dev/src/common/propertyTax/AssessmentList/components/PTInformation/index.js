3c7b40455ad1319c59817278a9c472b6f7975a6f
Ashok <ashok.kumar@egovernments.org>
2020-12-09 15:20:31 +0530

    PT Gender Change during transfer of ownership with multi owners

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/AssessmentList/components/PTInformation/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/AssessmentList/components/PTInformation/index.js
@@ -71,8 +71,8 @@ class PTInformation extends React.Component {
      // logoUrl = get(properties, "tenantId") ? this.getLogoUrl(get(properties, "tenantId")) : "";
      logoUrl = window.location.origin + `/pb-egov-assets/${tenantid}/logo.png`;
       corpCity = `TENANT_TENANTS_${get(properties, "tenantId").toUpperCase().replace(/[.:-\s\/]/g, "_")}`;
-      const selectedCityObject = cities && cities.length > 0 && cities.filter(item => item.code === get(properties, "tenantId"));
-      ulbGrade = selectedCityObject ? get(selectedCityObject[0], "city.ulbType").toUpperCase() : "MUNICIPAL CORPORATION";
+      const selectedCityObject =cities && cities && cities.length > 0 && cities.filter(item => item.code === get(properties, "tenantId"));
+      ulbGrade = selectedCityObject ?  get(selectedCityObject[0], "city.ulbType").toUpperCase() : "MUNICIPAL CORPORATION";
       //ulbGrade= "MUNICIPAL CORPORATION";
     }
     let propertyExist = properties.hasOwnProperty('propertyDetails');
