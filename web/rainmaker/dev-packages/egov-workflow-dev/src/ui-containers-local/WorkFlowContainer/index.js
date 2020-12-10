819dcf0e03b8fa5c73d7cdaa9fca2161bd15db21
Rajanihn <rajnihn@gmail.com>
2020-12-10 11:21:15 +0530

    validation added

--- web/rainmaker/dev-packages/egov-workflow-dev/src/ui-containers-local/WorkFlowContainer/index.js
+++ web/rainmaker/dev-packages/egov-workflow-dev/src/ui-containers-local/WorkFlowContainer/index.js
@@ -320,7 +320,7 @@ class WorkFlowContainer extends React.Component {
         `Licenses[0].comment`,
         null
       );
-      if(!pattern.test(comments)){
+      if(comments && !pattern.test(comments)){ 
         toggleSnackbar(
           true,
           { labelName: "Please enter valid data", labelKey: "TL_VALID_FIELDS_ERROR_MSG"},
