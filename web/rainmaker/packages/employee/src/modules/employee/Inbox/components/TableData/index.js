c43f7122405d6a021a5af69e0e0db1c4ebc1ad4f
minju <minju.sarah@gmail.com>
2020-12-15 16:17:18 +0530

    Modified Regular Expression Code

--- web/rainmaker/packages/employee/src/modules/employee/Inbox/components/TableData/index.js
+++ web/rainmaker/packages/employee/src/modules/employee/Inbox/components/TableData/index.js
@@ -360,7 +360,7 @@ class TableData extends Component {
       })
       var sla = item.businesssServiceSla && item.businesssServiceSla / (1000 * 60 * 60 * 24);
       let row0 = { text: item.businessId, subtext: item.businessService, hiddenText: item.moduleName };
-      let row1 = { text: locality ? <Label label={`${item.tenantId.toUpperCase().replace(/[.]/g, "_")}_REVENUE_${locality.locality}`} color="#000000" /> : <Label label={"NA"} color="#000000" /> };
+      let row1 = { text: locality ? <Label label={`${item.tenantId.toUpperCase().replace(/[.]/g, "_")}_REVENUE_${locality.locality.toUpperCase().replace(/[. ]/g, "_")}`} color="#000000" /> : <Label label={"NA"} color="#000000" /> };
       let row2 = {
         text: item.state ? (
           <Label
