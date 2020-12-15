4d0afdefc65c273da219872157551e2d8a0383bd
vamshikrishnakole-wtt-egov <57343517+vamshikrishnakole-wtt-egov@users.noreply.github.com>
2020-12-15 17:21:26 +0530

    RAIN-1931 (UI Update the Collection service API URL to new URL with business Service) (#2100)

--- web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/screens/specs/oc-bpa/search-preview.js
+++ web/rainmaker/dev-packages/egov-bpa-dev/src/ui-config/screens/specs/oc-bpa/search-preview.js
@@ -245,10 +245,28 @@ const setDownloadMenu = async (action, state, dispatch, applicationNumber, tenan
     }
   }
 
-  let paymentPayload = await httpRequest(
-    "post",
-    `collection-services/payments/_search?tenantId=${tenantId}&consumerCodes=${applicationNumber}`
-  );
+  let queryObject = [
+    {
+      key: "tenantId",
+      value: tenantId
+    },
+    {
+      key: "consumerCodes",
+      value: applicationNumber
+    }
+  ];
+  let paymentPayload = {}; 
+  paymentPayload.Payments = [];
+  let businessServicesList = ["BPA.NC_OC_APP_FEE", "BPA.NC_OC_SAN_FEE" ];
+    for(let fee = 0; fee < businessServicesList.length; fee++ ) {
+      let lowAppPaymentPayload = await httpRequest(
+        "post",
+        getPaymentSearchAPI(businessServicesList[fee]),
+        "",
+        queryObject
+      );
+      if(lowAppPaymentPayload && lowAppPaymentPayload.Payments && lowAppPaymentPayload.Payments.length > 0) paymentPayload.Payments.push(lowAppPaymentPayload.Payments[0]);
+    }
 
   if (paymentPayload && paymentPayload.Payments.length == 1) {
     if (get(paymentPayload, "Payments[0].paymentDetails[0].businessService") === "BPA.NC_OC_APP_FEE") {
