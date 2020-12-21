f546d82fd761be8ef2e09e5f9b0b7c2981b0ec88
Jagankumar-Egov <jagan.kumar@egovernments.org>
2020-12-21 09:54:53 +0530

    RAIN-1978 : PT Interminnent Issue while searching

--- web/rainmaker/dev-packages/egov-pt-dev/src/ui-config/screens/specs/pt-mutation/publicSearchResource/search-methods.js
+++ web/rainmaker/dev-packages/egov-pt-dev/src/ui-config/screens/specs/pt-mutation/publicSearchResource/search-methods.js
@@ -52,7 +52,7 @@ const searchApiCall = async (state, dispatch) => {
 
   let searchScreenObject = get(
     state.screenConfiguration.preparedFinalObject,
-    "searchScreen",
+    "ptSearchScreen",
     {}
   );
   
@@ -182,6 +182,11 @@ export const resetFields = (state, dispatch) => {
   dispatch(
     prepareFinalObject("applyScreenMdmsData.tenant.localities", [])
   );
+  dispatch(prepareFinalObject("ptSearchScreen.tenantId", ""));
+  dispatch(prepareFinalObject("ptSearchScreen.locality.code", ""));
+  dispatch(prepareFinalObject("ptSearchScreen.ids", ""));
+  dispatch(prepareFinalObject("ptSearchScreen.mobileNumber", ""));
+  dispatch(prepareFinalObject("ptSearchScreen.ownerName", ""));
   dispatch(prepareFinalObject("searchScreen.tenantId", ""));
   dispatch(prepareFinalObject("searchScreen.locality.code", ""));
   dispatch(prepareFinalObject("searchScreen.ids", ""));
