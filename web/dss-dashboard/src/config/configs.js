797551a97bd0ddcbdc1c02c7622a657cf996cb83
saumya-egov <74046603+saumya-egov@users.noreply.github.com>
2020-12-23 13:28:02 +0530

    Update configs.js

--- web/dss-dashboard/src/config/configs.js
+++ web/dss-dashboard/src/config/configs.js
@@ -10,7 +10,8 @@ const configs = {
     'MDMS': '/egov-mdms-service/v1/_search',
     'SHORTEN_URL':'/egov-url-shortening/shortener',
     'DOC_EXTENSION':['_small','_medium','_large'],
-    'CHART_COLOR_CODE':["#35a2eb", "#f19c56", "#4c76c7", "#ff6384", '#FFC107', '#009688', '#9C27B0', '#4CAF50',"#99d4fa", "#179cf4", "#1d9cf4", "#1sacq4", "#1gvcf4","ff8585","54e346","af2d2d","892cdc","aa3a3a","fcf876","f9813a","bedbbb","fa7f72","394867","e6739f","f6830f","0e918c","2d6187","a8dda8","ffe05d","b8de6f"],
+    'CHART_COLOR_CODE':["#35a2eb", "#f19c56", "#4c76c7", "#ff6384", "#FFC107", "#009688", "#9C27B0", "#4CAF50","#99d4fa", "#179cf4", "#1d9cf4", "#1sacq4", "#1gvcf4","#ff8585","#54e346","#af2d2d","#892cdc","#aa3a3a","#fcf876","#f9813a","#bedbbb","#fa7f72","#394867","#e6739f","#f6830f","#0e918c","#2d6187","#a8dda8","#ffe05d","#b8de6f","#0f3057","#ffeadb","#e6b566","#e8e9a1","#070d59","#54e346","#ffd369","#edf285","#fdcfdf","#965d62","#f2d974","#f1ae89","#ffe0d8"
+],
     'MODULE_LEVEL':[
                     {'dashboard':null},
                     {'tradelicense':{'services_name':'Trade licence','filterKey':'TL'}}
