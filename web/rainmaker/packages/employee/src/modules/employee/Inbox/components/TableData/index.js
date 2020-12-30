0a6ead78e68d8e12bea257651a8f189126929bde
Rajanihn <rajnihn@gmail.com>
2020-12-30 15:03:24 +0530

    Update

--- web/rainmaker/packages/employee/src/modules/employee/Inbox/components/TableData/index.js
+++ web/rainmaker/packages/employee/src/modules/employee/Inbox/components/TableData/index.js
@@ -295,8 +295,7 @@ class TableData extends Component {
     const businessIds = data.map((item) => {
       return item.businessId;
     });
-    let businessServiceData = [];
-    businessServiceData = this.getBussinessServiceData();
+    const businessServiceData = this.getBussinessServiceData();
     const modules =
       businessServiceData &&
       businessServiceData.map((item, index) => {
@@ -485,7 +484,7 @@ class TableData extends Component {
     const inboxData = [{ headers: [], rows: [] }];
     try {
       this.showLoading();
-      this.setBusinessServiceDataToLocalStorage([{ key: "tenantId", value: getTenantId() }]);
+      await this.setBusinessServiceDataToLocalStorage([{ key: "tenantId", value: getTenantId() }]);
       const requestBody = [{ key: "tenantId", value: tenantId }];
       const responseData = await httpRequest("egov-workflow-v2/egov-wf/process/_search", "_search", requestBody);
       const assignedData = orderBy(
