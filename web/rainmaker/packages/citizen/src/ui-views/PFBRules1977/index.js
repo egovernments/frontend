d3e1da9a6a33a90a3c6a9ca7bf4ddb8ef101e566
egovmis <42794267+egovmis@users.noreply.github.com>
2021-01-04 10:43:58 +0530

    Noc_Document (#2131)
    Co-authored-by: pmidc-egov-tech <55867523+pmidc-egov-tech@users.noreply.github.com>

---
+++ web/rainmaker/packages/citizen/src/ui-views/PFBRules1977/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const PFBRules1977 = (props) => {
+  return (
+    <div style={{ height: "100vh" }}>
+      <iframe
+        src="https://firenoc.lgpunjab.gov.in/downloads/PunjabFireBrigadeRules1977.pdf#view=FitH&embedded=true"
+        style={{ width: "100%", height: "90%" }}
+        frameborder="0"
+      />
+      {/* <p>
+        Your web browser doesn't have a PDF plugin. Instead you can{" "}
+        <a href="https://s3.ap-south-1.amazonaws.com/pb-egov-assets/pb/TL_UserManual_Citizen.pdf">click here to download the PDF file.</a>
+      </p> */}
+    </div>
+  );
+};
+
+export default PFBRules1977;
