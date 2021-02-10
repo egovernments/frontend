d4574cd462b2bdbab357edb914dae900c325ab0f
Vidyabhushan300 <vidyaisbhushan@gmail.com>
2021-02-10 16:23:34 +0530

    same as property address field hidden, for employee CB sleection disabled.

--- web/rainmaker/dev-packages/egov-pt-dev/src/ui-config/screens/specs/pt-common-screens/summaryResource/propertyOwnershipSummary.js
+++ web/rainmaker/dev-packages/egov-pt-dev/src/ui-config/screens/specs/pt-common-screens/summaryResource/propertyOwnershipSummary.js
@@ -161,20 +161,20 @@ export const applicantSummary = getCommonGrayCard({
             callBack: checkValueForNA
           }
         ),
-        reviewSameAsPropertyAddress: getLabelWithValue(
-          {
-            labelName: "Same as property address",
-            labelKey: "PT_COMMON_SAME_AS_PROPERTY_ADDRESS"
-          },
-          {
-            jsonPath: "Property.owners[0].sameAsPeropertyAddress",
-            localePrefix: {
-              moduleName: "common-masters",
-              masterName: "OwnerType"
-            },
-            callBack: checkValueForNA
-          }
-        ),
+        // reviewSameAsPropertyAddress: getLabelWithValue(
+        //   {
+        //     labelName: "Same as property address",
+        //     labelKey: "PT_COMMON_SAME_AS_PROPERTY_ADDRESS"
+        //   },
+        //   {
+        //     jsonPath: "Property.owners[0].sameAsPeropertyAddress",
+        //     localePrefix: {
+        //       moduleName: "common-masters",
+        //       masterName: "OwnerType"
+        //     },
+        //     callBack: checkValueForNA
+        //   }
+        // ),
         break: getBreak()
       }),
       items: [],
@@ -311,16 +311,16 @@ export const institutionSummary = getCommonGrayCard({
             callBack: checkValueForNA
           }
         ),
-        sameAsPropertyAddress: getLabelWithValue(
-          {
-            labelName: "Same as property address",
-            labelKey: "PT_COMMON_SAME_AS_PROPERTY_ADDRESS"
-          },
-          {
-            jsonPath: "Property.owners[0].sameAsPeropertyAddress",
-            callBack: checkValueForNA
-          }
-        ),
+        // sameAsPropertyAddress: getLabelWithValue(
+        //   {
+        //     labelName: "Same as property address",
+        //     labelKey: "PT_COMMON_SAME_AS_PROPERTY_ADDRESS"
+        //   },
+        //   {
+        //     jsonPath: "Property.owners[0].sameAsPeropertyAddress",
+        //     callBack: checkValueForNA
+        //   }
+        // ),
         break: getBreak()
       }),
       items: [],
