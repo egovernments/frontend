9c52f422819f061576ec630ab0ef36896c8bcd40
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2021-02-17 19:15:31 +0530

    Revert "RAIN-2247 : Bill Amend Added a New Taxhead changes"
    This reverts commit c937cc6d439fcb678b021dce33647392961f6fdb.

--- web/rainmaker/dev-packages/egov-billamend-dev/src/ui-config/screens/specs/bill-amend/apply.js
+++ web/rainmaker/dev-packages/egov-billamend-dev/src/ui-config/screens/specs/bill-amend/apply.js
@@ -188,7 +188,7 @@ export const getMdmsData = async (action, state, dispatch) => {
     let taxHeadMasterMdmsDetails = get(payload, "MdmsRes.BillingService.TaxHeadMaster", []), taxHeadMasterDetails;
     if (taxHeadMasterMdmsDetails && taxHeadMasterMdmsDetails.length > 0) {
       taxHeadMasterDetails = taxHeadMasterMdmsDetails.filter(service => (service.service == businessService));
-      let billTaxHeadMasterDetails = taxHeadMasterDetails.filter(data => (data.code.includes("AMENDMENT")));
+      let billTaxHeadMasterDetails = taxHeadMasterDetails.filter(data => (data.IsBillamend== true));
       if(billTaxHeadMasterDetails && billTaxHeadMasterDetails.length > 0) {
        billTaxHeadMasterDetails.map(bill => {
           bill.reducedAmountValue = 0;
