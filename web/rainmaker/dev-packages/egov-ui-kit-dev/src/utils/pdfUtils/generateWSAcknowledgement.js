52bcdce74406ff5e6844be4af932f3620fea37b3
vamshikrishnakole-wtt-egov <vamshikrishna.kole@egovernments.org>
2021-02-19 10:15:48 +0530

    RAIN-2223 (W&S : Multiple road type support)

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/pdfUtils/generateWSAcknowledgement.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/utils/pdfUtils/generateWSAcknowledgement.js
@@ -100,7 +100,7 @@ export const generateWSAcknowledgement = (preparedFinalObject, fileName = "print
             { header: 'PDF_STATIC_LABEL_WS_CONSOLIDATED_DOCUMENTS_DETAILS_HEADER', items: documentCard, hide: documentCard.length === 0 },
             { header: 'PDF_STATIC_LABEL_WS_CONSOLIDATED_ACKNOWELDGMENT_ADDITIONAL_CONNECTION_HEADER', items: additionDetail },
             { header: 'PDF_STATIC_LABEL_WS_CONSOLIDATED_ACKNOWELDGMENT_PLUMBER_DETAILS_HEADER', items: plumberDetail, hide: plumberDetail.length === 0 },
-            { header: 'PDF_STATIC_LABEL_WS_CONSOLIDATED_ACKNOWELDGMENT_ROAD_CHARGES_HEADER', items: roadDetail, type: roadDetailInfo.length > 1 ? 'multiItem' : 'singleItem',  hide: roadDetailInfo.length === 0},
+            { header: 'PDF_STATIC_LABEL_WS_CONSOLIDATED_ACKNOWELDGMENT_ROAD_CHARGES_HEADER', items: roadDetailInfo, type: roadDetailInfo.length > 1 ? 'multiItem' : 'singleItem',  hide: roadDetailInfo.length === 0},
             { header: 'PDF_STATIC_LABEL_WS_CONSOLIDATED_ACKNOWELDGMENT_ACTIVATION_DETAILS_HEADER', items: activateDetail },
             { header: 'PDF_STATIC_LABEL_WS_CONSOLIDATED_ACKNOWELDGMENT_MODIFY_EFFECTIVE_DATE_HEADER', items: reviewModificationsEffective, hide: reviewModificationsEffective.length === 0 },
 
