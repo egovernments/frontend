d3ffeab689c1ce66e0c40db65bf2a6dc42b1191a
Ashok <ashok.kumar@egovernments.org>
2021-02-23 14:05:28 +0530

    apply screen popup document issue

--- web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-utils/commons.js
+++ web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-utils/commons.js
@@ -698,10 +698,11 @@ export const getRequiredDocData = async (action, dispatch, moduleDetails, closeP
     process.env.REACT_APP_NAME === "Citizen" ? JSON.parse(getUserInfo()).permanentCity : getTenantId();
   let mdmsBody = {
     MdmsCriteria: {
-      tenantId: moduleDetails[0].moduleName === "ws-services-masters" ? commonConfig.tenantId : tenantId,
+      tenantId: moduleDetails[0].moduleName === "ws-services-masters" || moduleDetails[0].moduleName === "PropertyTax" ? commonConfig.tenantId : tenantId,
       moduleDetails: moduleDetails
     }
   };
+  debugger;
   try {
     let payload = null;
     payload = await httpRequest(
