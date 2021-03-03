e5a87af0dff4c9a069bbe94cfa06071ed6cd05ec
Subhash-eGov <subhash.bose@egovernments.org>
2021-03-03 11:12:58 +0530

    FINANCE-157: sub domain formation logic change to form new production sub domain url (#2292)

--- web/rainmaker/packages/employee/src/modules/employee/Erp/EGF/index.js
+++ web/rainmaker/packages/employee/src/modules/employee/Erp/EGF/index.js
@@ -39,8 +39,8 @@ class EGFFinance extends Component {
       erp_url = loc.protocol + "//" + getTenantId().split(".")[1] + "-" + subdomainurl + menuUrl;
     } else {
       subdomainurl = hostname.substring(hostname.indexOf(".") + 1);
-      erp_url = loc.protocol + "//" + getTenantId().split(".")[1] + "." + subdomainurl + menuUrl;
-    }
+      erp_url = loc.protocol + "//" + getTenantId().split(".")[1] + "-prod" + "." + subdomainurl + menuUrl;
+   }
 
     // let erp_url='http://jalandhar.test.egov.com:8080'+menuUrl;
     console.log("ERP URL : " + erp_url);
