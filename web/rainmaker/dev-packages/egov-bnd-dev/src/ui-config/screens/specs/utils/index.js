fa46dae1db47c957be4dc9684b42f3f6b73453d3
SrikanthVH <shrknth@gmail.com>
2021-03-09 18:29:15 +0530

    Sycing missed commit -  merged, frontend-dev and bnd
    Sycing missed commit -  merged, frontend-dev and bnd

--- web/rainmaker/dev-packages/egov-bnd-dev/src/ui-config/screens/specs/utils/index.js
+++ web/rainmaker/dev-packages/egov-bnd-dev/src/ui-config/screens/specs/utils/index.js
@@ -568,12 +568,12 @@ export const downloadCert = async (tenantId, id) => {
 }
 
 
-export const postPaymentSuccess = async(action,state,dispatch, data) => {
+export const postPaymentSuccess = async(data) => {
 
-  dispatch(toggleSpinner());
+  store.dispatch(toggleSpinner());
   setTimeout(() => {     
-    postPaymentActivity(action,state, dispatch,data);
-    dispatch(toggleSpinner());
+    postPaymentActivity(data);
+    store.dispatch(toggleSpinner());
   }, 4000); //Give 2 sec gap so that the screen is loaded correctly
     
 };
