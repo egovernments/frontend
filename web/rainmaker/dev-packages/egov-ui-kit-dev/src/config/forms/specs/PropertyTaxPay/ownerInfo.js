ac100ce81b8530caa4d37a5615be1cbf83d6246b
Jagankumar-Egov <jagan.kumar@egovernments.org>
2021-03-16 18:10:42 +0530

    RAIN-2352 : PT Relationship field issue incase of remove value and proceed

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/config/forms/specs/PropertyTaxPay/ownerInfo.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/config/forms/specs/PropertyTaxPay/ownerInfo.js
@@ -64,6 +64,7 @@ const formConfig = {
     },
     ownerRelationship: {
       id: "ownerRelationship",
+      required: true,
       jsonPath: "Properties[0].propertyDetails[0].owners[0].relationship",
       type: "AutocompleteDropdown",
       localePrefix: "PT_RELATION",
