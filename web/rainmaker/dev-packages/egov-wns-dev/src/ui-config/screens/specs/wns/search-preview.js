d0b98cfb6547d5b2b37dbfc29c08855612cf22c4
rakesh-wt-egov <rakesh.katagouni@egovernments.org>
2021-03-18 17:03:44 +0530

    PI-1374 W&S::Fazilka::QA:: Extra field is displayed in summary page for additional details section which is not displayed in additional details page

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/search-preview.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/search-preview.js
@@ -281,19 +281,17 @@ const beforeInitFn = async (action, state, dispatch, applicationNumber) => {
         )
       );
     }
-    // let unitUsageTypee = await get(state, "screenConfiguration.preparedFinalObject.SewerageConnection.additionalDetails.unitUsageType");
-    // debugger;
-    // console.log("================unitUsageTypee",unitUsageTypee);
-    // if(unitUsageTypee === null||unitUsageTypee === undefined ) {
-    //   dispatch(
-    //     handleField(
-    //       "apply", 
-    //       "components.div.children.formwizardFourthStep.children.summaryScreen.children.cardContent.children.reviewOwnerDetails.children.cardContent.children.viewSix.children.reviewUnitUsageType",
-    //        "visible",
-    //        false
-    //     )
-    //   );
-    // }
+    let unitUsageTypee = get(state, "screenConfiguration.preparedFinalObject.WaterConnection[0].property.usageCategory");
+    if(unitUsageTypee != "MIXED" ) {
+      dispatch(
+        handleField(
+          "search-preview", 
+          "components.div.children.taskDetails.children.cardContent.children.reviewOwnerDetails.children.cardContent.children.viewSixWS.children.reviewUnitUsageType",
+           "visible",
+           false
+        )
+      );
+    }
     let connectionType = get(state, "screenConfiguration.preparedFinalObject.WaterConnection[0].connectionType");
     if (connectionType === "Metered") {
       set(
