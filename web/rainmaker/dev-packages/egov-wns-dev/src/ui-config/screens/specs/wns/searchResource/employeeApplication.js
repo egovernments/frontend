8c2d4702984973f6233b637fe422c33df0b590ec
Ashok <ashok.kumar@egovernments.org>
2021-03-24 16:10:47 +0530

    PI-9171: mseva-qa-ahmedgarh : Old connection Number Field Related

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/searchResource/employeeApplication.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/searchResource/employeeApplication.js
@@ -114,7 +114,7 @@ export const wnsApplication = getCommonCard({
             sm: 4
         },
         required: false,
-        pattern: /^[a-zA-Z0-9-]*$/i,
+        // pattern: /^[a-zA-Z0-9-]*$/i,
         errorMessage: "ERR_DEFAULT_INPUT_FIELD_MSG",
         jsonPath: "searchConnection.oldConnectionNumber"
     })
