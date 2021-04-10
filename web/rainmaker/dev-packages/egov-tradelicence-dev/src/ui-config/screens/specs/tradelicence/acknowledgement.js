33288a17af628fc5083640eb1c66f0475b284bf0
Ashok <ashok.kumar@egovernments.org>
2021-04-11 02:24:38 +0530

    proceed to Payment button isssue in TL.

--- web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/tradelicence/acknowledgement.js
+++ web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/tradelicence/acknowledgement.js
@@ -709,7 +709,9 @@ const screenConfig = {
       tenant
     );
     set(action, "screenConfig.components.div.children", data);
-    if( purpose === "forward") {
+    const status1 =get(state.screenConfiguration.preparedFinalObject, "Licenses[0].status" );
+    const applicationType =get(state.screenConfiguration.preparedFinalObject, "Licenses[0].applicationType" );
+    if( status1 === "APPLIED" || status1 === "APPLIED" &&applicationType==="RENEWAL" ) {
       dispatch(
         handleField(
           "acknowledgement",
