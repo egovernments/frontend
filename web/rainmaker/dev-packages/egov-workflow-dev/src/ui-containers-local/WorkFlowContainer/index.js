9d011fd473d289fdccb19f7ef557027b4d139c86
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2021-04-16 17:37:34 +0530

    RAIN-2251 : PT Update Business service and MDMS integration for it (#2378)
    * RAIN-2251 : PT Workflow updated to PT.UPDATE
    
    * RAIN-2251 : PT Update Business service and MDMS integration for it
    
    * RAIN-2251 : PT Update Proeprty Config added in WF component

--- web/rainmaker/dev-packages/egov-workflow-dev/src/ui-containers-local/WorkFlowContainer/index.js
+++ web/rainmaker/dev-packages/egov-workflow-dev/src/ui-containers-local/WorkFlowContainer/index.js
@@ -240,7 +240,7 @@ class WorkFlowContainer extends React.Component {
       if (payload) {
         let path = "";
         this.props.hideSpinner();
-        if (moduleName == "PT.CREATE" || moduleName == "PT.LEGACY") {
+        if (moduleName == "PT.CREATE" ||moduleName == "PT.UPDATE" || moduleName == "PT.LEGACY") {
           this.props.setRoute(`/pt-mutation/acknowledgement?${this.getPurposeString(
             label
           )}&moduleName=${moduleName}&applicationNumber=${get(payload, 'Properties[0].acknowldgementNumber', "")}&tenantId=${get(payload, 'Properties[0].tenantId', "")}`);
@@ -365,7 +365,7 @@ class WorkFlowContainer extends React.Component {
       }
     } else if (moduleName === "PT") {
       bservice = "PT"
-    } else if (moduleName === "PT.CREATE" || moduleName === "PT.LEGACY") {
+    } else if (moduleName === "PT.CREATE" || moduleName === "PT.UPDATE" || moduleName === "PT.LEGACY") {
       return `/property-tax/assessment-form?assessmentId=0&purpose=update&propertyId=${propertyId}&tenantId=${tenant}&mode=WORKFLOWEDIT`
     } else if (moduleName === "PT.MUTATION") {
       bservice = "PT.MUTATION";
