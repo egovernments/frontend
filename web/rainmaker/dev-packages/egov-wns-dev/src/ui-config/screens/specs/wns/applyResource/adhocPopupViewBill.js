1367563380c49e4ce0c1e5bbca88248f94f71d17
vamshikrishnakole-wtt-egov <vamshikrishna.kole@egovernments.org>
2021-04-22 12:47:00 +0530

    RDS-141 added cosumer code for add rebate penality

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/applyResource/adhocPopupViewBill.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/applyResource/adhocPopupViewBill.js
@@ -41,6 +41,7 @@ const getEstimateDataAfterAdhoc = async (state, dispatch) => {
       "viewBillToolipData[0].description.demandId"
     );
 
+  const consumerCode = getQueryArg(window.location.href, "connectionNumber") || WSRequestBody[0].connectionNo;
   let serviceUrl,httpmethod;
   if (WSRequestBody[0].service === serviceConst.WATER) {
     serviceUrl = "ws-calculator/waterCalculator/_applyAdhocTax";
@@ -58,7 +59,8 @@ try{
     {
       "demandId":demandId,
       "adhocrebate" : (WSRequestBody[0].additionalDetails.adhocRebate)?WSRequestBody[0].additionalDetails.adhocRebate:0,
-      "adhocpenalty" : (WSRequestBody[0].additionalDetails.adhocPenalty)?WSRequestBody[0].additionalDetails.adhocPenalty:0
+      "adhocpenalty" : (WSRequestBody[0].additionalDetails.adhocPenalty)?WSRequestBody[0].additionalDetails.adhocPenalty:0,
+      "consumerCode": consumerCode
     }
   );
 
