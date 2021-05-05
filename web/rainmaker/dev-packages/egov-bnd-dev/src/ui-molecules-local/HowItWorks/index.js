9df5d5d1115db8c7c1e8d59383e9880c5c6862b3
MinjuBony <66290009+MinjuBony@users.noreply.github.com>
2021-05-05 15:03:55 +0530

    Bug Fix

--- web/rainmaker/dev-packages/egov-bnd-dev/src/ui-molecules-local/HowItWorks/index.js
+++ web/rainmaker/dev-packages/egov-bnd-dev/src/ui-molecules-local/HowItWorks/index.js
@@ -15,7 +15,7 @@ const gethelpURL=()=>{
     hostname  = "https://13.71.65.215.nip.io";
   else  
     hostname  = window.location.origin;
-  const url=new URL(hostname+"/filestore/v1/files/static?fileStoreId=BirthDeathUserManual_Citizen");
+  const url=new URL(hostname+"/filestore/v1/files/static?fileStoreId=BirthDeathUserManual_Citizen.pdf");
   return url;
 
 }
