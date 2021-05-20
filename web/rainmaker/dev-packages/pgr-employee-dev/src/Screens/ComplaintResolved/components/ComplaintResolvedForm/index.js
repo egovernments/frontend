b2533d4c8fdafb4d7721b60b600e0f6b9bda759a
mohaarif007 <48949208+mohaarif007@users.noreply.github.com>
2021-05-20 13:34:18 +0530

    Other api call on  Resolve complain (#2421)
    Other api call on  Resolve complain

--- web/rainmaker/dev-packages/pgr-employee-dev/src/Screens/ComplaintResolved/components/ComplaintResolvedForm/index.js
+++ web/rainmaker/dev-packages/pgr-employee-dev/src/Screens/ComplaintResolved/components/ComplaintResolvedForm/index.js
@@ -4,6 +4,22 @@ import { ImageUpload } from "modules/common";
 import { TextArea } from "modules/common";
 
 const ComplaintResolvedForm = ({ formKey, form, handleFieldChange, onSubmit }) => {
+  function callDgrApi() {
+   var complaintId = (window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1)).replace(/%2F/gi, "/");
+   var request = require('request');
+   var options = {
+    'method': 'POST',
+    'url': 'http://devgrievanceapi.psegs.in/api/grievance/GetComplaintStatus_PMIDC',
+    'headers': {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({"Complaint_Id":complaintId,"Remarks":"Resolved Succesffully","Status":"resolved"})
+    };
+    request(options, function (error, response) {
+    if (error)throw new Error(error);
+    console.log(response.body);
+    });
+  }
   const fields = form.fields || {};
   const submit = form.submit;
   return (
@@ -16,7 +32,7 @@ const ComplaintResolvedForm = ({ formKey, form, handleFieldChange, onSubmit }) =
       </div>
       <div className="responsive-action-button-cont">
         <Button
-          onClick={onSubmit}
+          onClick={onSubmit;callDgrApi();}
           className="responsive-action-button"
           id={"complaint-resolved-mark-resolved"}
           {...submit}
