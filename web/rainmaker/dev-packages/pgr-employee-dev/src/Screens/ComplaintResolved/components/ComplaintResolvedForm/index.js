3f74a1165c49f099a3af50e93d75ed1e2e017315
Ashok <ashok.kumar@egovernments.org>
2021-05-20 17:36:09 +0530

    Removing Aarif changes as it is giving error at build time

--- web/rainmaker/dev-packages/pgr-employee-dev/src/Screens/ComplaintResolved/components/ComplaintResolvedForm/index.js
+++ web/rainmaker/dev-packages/pgr-employee-dev/src/Screens/ComplaintResolved/components/ComplaintResolvedForm/index.js
@@ -4,22 +4,22 @@ import { ImageUpload } from "modules/common";
 import { TextArea } from "modules/common";
 
 const ComplaintResolvedForm = ({ formKey, form, handleFieldChange, onSubmit }) => {
-  function callDgrApi() {
-    var complaintId = (window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1)).replace(/%2F/gi, "/");
-    var request = require('request');
-    var options = {
-      'method': 'POST',
-      'url': 'http://devgrievanceapi.psegs.in/api/grievance/GetComplaintStatus_PMIDC',
-      'headers': {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ "Complaint_Id": complaintId, "Remarks": "Resolved Succesffully", "Status": "resolved" })
-    };
-    request(options, function (error, response) {
-      if (error) throw new Error(error);
-      console.log(response.body);
-    });
-  }
+  // function callDgrApi() {
+  //   var complaintId = (window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1)).replace(/%2F/gi, "/");
+  //   var request = require('request');
+  //   var options = {
+  //     'method': 'POST',
+  //     'url': 'http://devgrievanceapi.psegs.in/api/grievance/GetComplaintStatus_PMIDC',
+  //     'headers': {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({ "Complaint_Id": complaintId, "Remarks": "Resolved Succesffully", "Status": "resolved" })
+  //   };
+  //   request(options, function (error, response) {
+  //     if (error) throw new Error(error);
+  //     console.log(response.body);
+  //   });
+  // }
   const fields = form.fields || {};
   const submit = form.submit;
   return (
@@ -33,8 +33,8 @@ const ComplaintResolvedForm = ({ formKey, form, handleFieldChange, onSubmit }) =
       <div className="responsive-action-button-cont">
         <Button
           onClick={() => {
-            onSubmit();
-            callDgrApi();
+            onSubmit
+            // callDgrApi();
           }}
           className="responsive-action-button"
           id={"complaint-resolved-mark-resolved"}
