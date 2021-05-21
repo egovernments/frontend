8d36a17fcea335ce22e178e2ff0cd5316ffbac7d
Jagankumar-Egov <jagan.kumar@egovernments.org>
2021-05-21 17:12:35 +0530

    Revert "Filter related changes"
    This reverts commit e6823fed6e369b8d86be7c2ddc36822c942712af.

--- web/rainmaker/packages/employee/src/modules/employee/NewInbox/components/Filter/index.js
+++ web/rainmaker/packages/employee/src/modules/employee/NewInbox/components/Filter/index.js
@@ -5,11 +5,12 @@ import "./index.css";
 
 
 
-const Filter = ({ filter, handleChangeFilter, clearFilter ,applyFilter}) => {
+const Filter = ({ filter, handleChangeFilter, clearFilter }) => {
     return (
         <div className="row" style={{marginLeft:'-5px' ,    marginBottom: '15px'}}>
             <div className="col-md-3">
                 <MultiSelectDropdown
+                    multiple
                     onChange={(e) => { handleChangeFilter('moduleFilter', e.target.value) }}
                     floatingLabelText={<Label label="CS_INBOX_MODULE_FILTER" fontSize="12px" />}//"Module"
                     className="filter-fields"
@@ -26,7 +27,25 @@ const Filter = ({ filter, handleChangeFilter, clearFilter ,applyFilter}) => {
                 />
             </div>
             <div className="col-md-3">
-            <MultiSelectDropdown
+                <MultiSelectDropdown
+                    multiple
+                    onChange={(e, index, value) => {
+                        handleChangeFilter('localityFilter', e.target.value)
+                    }}
+                    floatingLabelText={<Label label="CS_INBOX_LOCALITY_FILTER" fontSize="12px"/>}
+                    className="filter-fields"
+                    dropDownData={filter.localityFilter.dropdownData}
+                    value={filter.localityFilter.selectedValue}
+                    underlineStyle={{
+                        position: "absolute",
+                        bottom: -1,
+                        borderBottom: "1px solid #FE7A51",
+                        width: "90%"
+                    }}
+                />
+            </div>
+            <div className="col-md-3" >
+                <MultiSelectDropdown
                  multiple
                     floatingLabelText={<Label label="CS_INBOX_STATUS_FILTER" fontSize="12px"  />}
                     className="filter-fields"
@@ -43,27 +62,6 @@ const Filter = ({ filter, handleChangeFilter, clearFilter ,applyFilter}) => {
                     }}
                     prefix ={"COMMON_"}
                 />
-                {/* <MultiSelectDropdown
-                    multiple
-                    onChange={(e, index, value) => {
-                        handleChangeFilter('localityFilter', e.target.value)
-                    }}
-                    floatingLabelText={<Label label="CS_INBOX_LOCALITY_FILTER" fontSize="12px"/>}
-                    className="filter-fields"
-                    dropDownData={filter.localityFilter.dropdownData}
-                    value={filter.localityFilter.selectedValue}
-                    underlineStyle={{
-                        position: "absolute",
-                        bottom: -1,
-                        borderBottom: "1px solid #FE7A51",
-                        width: "90%"
-                    }}
-                /> */}
-            </div>
-            <div className="col-md-3" >
-            <div className="rainmaker-displayInline filter-clear-icon" onClick={applyFilter} >
-                    <Label label="CS_INBOX_APPLY" color="#fe7a51" fontSize="15px" />
-                </div>
             </div>
             <div className="col-md-3">
                 <div className="rainmaker-displayInline filter-clear-icon" onClick={clearFilter} >
