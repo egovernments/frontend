c47b386caed92cbc7b1b07c5ee780fee5e5b5d3f
egovmis <mis.pmidc@punjab.gov.in>
2021-05-31 16:20:34 +0530

    changes for go back button after payment

--- web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/acknowledgementResource/paymentFooter.js
+++ web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/acknowledgementResource/paymentFooter.js
@@ -13,6 +13,17 @@ const getHomeButtonPath = (item) => {
     }
     return isPublicSearch() ? url : (ifUserRoleExists("CITIZEN") ? get(item, "citizenUrl", "/") : get(item, "employeeUrl", "/inbox"));
 }
+const getBackButtonPath = (item) => {
+    const consumerCode = getQueryArg(window.location.href, "consumerCode");
+    let url = "/withoutAuth/pt-mutation/public-search";
+    if(consumerCode.includes("UC")) {
+        url = "/withoutAuth/uc/newCollection";
+
+        return isPublicSearch() ? url : (ifUserRoleExists("CITIZEN") ? get(item, "citizenUrl", "/") : get(item, "employeeUrl", "/uc/newCollection"));
+
+    }
+    //return isPublicSearch() ? url : (ifUserRoleExists("CITIZEN") ? get(item, "citizenUrl", "/") : get(item, "employeeUrl", "/inbox"));
+}
 
 const getCommonApplyFooter = (children) => {
     return {
@@ -41,9 +52,14 @@ const defaultValues = {
 
 export const paymentFooter = (state, consumerCode, tenant, status, businessService, extraData) => {
     const uiCommonPayConfig = get(state.screenConfiguration.preparedFinalObject, "commonPayInfo", defaultValues);
-    const buttons = get(uiCommonPayConfig, "buttons");
+    const buttons = get(uiCommonPayConfig[0], "buttons");
     const redirectionURL = isPublicSearch() ? "/withoutAuth/egov-common/pay" : "/egov-common/pay";
-    const path = `${redirectionURL}?consumerCode=${consumerCode}&tenantId=${tenant}&businessService=${businessService}`
+    const path = `${redirectionURL}?consumerCode=${consumerCode}&tenantId=${tenant}&businessService=${businessService}`;
+    
+    let gobackPath="/inbox";
+    if(consumerCode.includes("UC")){
+        gobackPath = isPublicSearch() ? "/withoutAuth/uc/search" : "/uc/search";
+    }
     setTimeout(function(){if(extraData!=null){
         if(extraData.payment.paymentDetails[0].businessService=="PT"){
           if (window.appOverrides && window.appOverrides.validateForm)
@@ -117,8 +133,58 @@ export const paymentFooter = (state, consumerCode, tenant, status, businessServi
             },
         }
     })
+
+    // const goBack = buttons && buttons.map((item, index) => {
+    //     return {
+    //         componentPath: "Button",
+    //         props: {
+    //             variant: "contained",
+    //             color: "primary",
+    //             className: "common-footer-mobile",
+    //             style: {
+    //                 minWidth: "200px",
+    //                 height: "48px",
+    //                 marginRight: "16px",
+    //                 marginLeft: "40px"
+    //             }
+    //         },
+    //         children: {
+    //             downloadReceiptButtonLabel: getLabel({
+    //                 labelKey: get(item, "label", "GO Back")
+    //             })
+    //         },
+    //         onClickDefination: {
+    //             action: "page_change",
+    //             path: getBackButtonPath(item)
+    //         },
+    //     }
+    // })
     return getCommonApplyFooter({
         ...footer,
+        goBackButton: {
+            componentPath: "Button",
+            props: {
+                variant: "contained",
+                color: "primary",
+                className: "common-footer-mobile",
+                style: {
+                    minWidth: "200px",
+                    height: "48px",
+                    marginRight: "16px",
+                    marginLeft: "40px",
+                }
+            },
+            children: {
+                downloadReceiptButtonLabel: getLabel({
+                    labelName: "Go Back",
+                    labelKey: "Go Back"
+                })
+            },
+            onClickDefination: {
+                action: "page_change",
+                path : gobackPath
+            }
+        },
         retryButton: {
             componentPath: "Button",
             props: {
