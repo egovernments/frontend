672e9c3796ee0a2359aac63fe77ce6926b0dfe9d
Jagankumar-Egov <jagan.kumar@egovernments.org>
2021-07-06 13:15:14 +0530

    Update index.js

--- web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-config/screens/specs/utils/index.js
+++ web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-config/screens/specs/utils/index.js
@@ -582,7 +582,7 @@ export const getPattern = type => {
     case "GSTNo":
       return /^\d{2}[A-Z]{5}\d{4}[A-Z]{1}\d[Z]{1}[A-Z\d]{1}$/i;
     case "DoorHouseNo":
-      return /^[^\$\"'<>?\\\\~`!@$%^={}\[\]*:;“”‘’]{1,50}$/i;
+      return /^[^\$\"'<>?~`!@$%^={}\[\]*:;“”‘’]{1,50}$/i;
     case "BuildingStreet":
       return /^[^\$\"'<>?\\\\~`!@$%^()+={}\[\]*.:;“”‘’]{1,64}$/i;
     case "Pincode":
