8711653673ad3bb62de9a1c3c605a3ee8fdb7a9b
Prasad Reddy <guruprasadreddy.narala@egovernments.org>
2021-07-14 17:28:02 +0530

    Open Search Header Changes

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/Header/components/UserSettings/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/common/Header/components/UserSettings/index.js
@@ -1,7 +1,7 @@
 import { DropDown, Icon, Image, List } from "components";
 import { getTransformedLocale, getLocaleLabels } from "egov-ui-framework/ui-utils/commons";
 import emptyFace from "egov-ui-kit/assets/images/download.png";
-import { getLocale, getTenantId, setTenantId, getUserInfo } from "egov-ui-kit/utils/localStorageUtils";
+import { getLocale, getTenantId, setTenantId, getUserInfo, setStoredModulesList, setModule } from "egov-ui-kit/utils/localStorageUtils";
 import React, { Component } from "react";
 import LogoutDialog from "../LogoutDialog";
 import { getQueryArg } from "egov-ui-kit/utils/commons";
@@ -9,7 +9,7 @@ import { CommonMenuItems } from "../NavigationDrawer/commonMenuItems";
 import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import { connect } from "react-redux";
 import get from "lodash/get";
-import { setRoute } from "egov-ui-kit/redux/app/actions";
+import { setRoute, setLocalizationLabels } from "egov-ui-kit/redux/app/actions";
 
 import "./index.css";
 
@@ -53,7 +53,7 @@ class UserSettings extends Component {
       background: "#ffffff",
       height: "65px",
       marginRight: "30px",
-      width: "120px",
+      width: "102px",
       marginBottom: "24px",
     },
   };
@@ -94,6 +94,12 @@ class UserSettings extends Component {
       tenantId = userInfo && userInfo.permanentCity;
       tenantId = tenantInfo?tenantInfo:tenantId;
     }
+    var resetList=[];
+    var newList =JSON.stringify(resetList);
+    setStoredModulesList(newList);
+    let locale= getLocale();
+    let resultArray=[];
+    setLocalizationLabels(locale, resultArray);
     this.props.fetchLocalizationLabel(value, tenantId, tenantId);
   };
 
@@ -120,7 +126,7 @@ class UserSettings extends Component {
   render() {
     const { languageSelected, displayAccInfo, tenantSelected, open } = this.state;
     const { style } = this;
-    const { onIconClick, userInfo, handleItemClick, hasLocalisation, languages, fetchLocalizationLabel } = this.props;
+    const { onIconClick, userInfo, handleItemClick, hasLocalisation, languages, fetchLocalizationLabel, isUserSetting } = this.props;
 
     /**
      * Get All tenant id's from (user info -> roles) to populate dropdown
@@ -135,7 +141,7 @@ class UserSettings extends Component {
 
     return (
       <div className="userSettingsContainer">
-        <LogoutDialog
+        {isUserSetting && <LogoutDialog
           logoutPopupOpen={open}
           closeLogoutDialog={this.handleClose}
           logout={this.handleTenantChange}
@@ -143,9 +149,8 @@ class UserSettings extends Component {
           canceltext={"CORE_CHANGE_TENANT_CANCEL"}
           title={"CORE_CHANGE_TENANT"}
           body={"CORE_CHANGE_TENANT_DESCRIPTION"}
-        />
-        { /* Commenting for 10 dec release
-        process.env.REACT_APP_NAME === "Employee" && (
+        />}
+        {process.env.REACT_APP_NAME === "Employee" && isUserSetting && (
           <DropDown
             onChange={this.onTenantChange}
             listStyle={style.listStyle}
@@ -155,8 +160,7 @@ class UserSettings extends Component {
             value={tenantSelected}
             underlineStyle={{ borderBottom: "none" }}
           />
-        )
-        */}
+        )}
         {hasLocalisation && (
           <DropDown
             onChange={this.onLanguageChange}
@@ -185,7 +189,7 @@ class UserSettings extends Component {
 
         {/* <Icon action="social" name="notifications" color="#767676" style={style.iconStyle} /> */}
         <ClickAwayListener onClickAway={this.handleClose}>
-          <div
+          {isUserSetting && <div
             onClick={() => {
               this.toggleAccInfo();
             }}
@@ -211,17 +215,18 @@ class UserSettings extends Component {
                   ""
                 )}
             </div>
-          </div>
+          </div>}
         </ClickAwayListener>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ common }) => {
+const mapStateToProps = ({ app,common }) => {
+  const {locale}=app;
   const { stateInfoById } = common;
   let languages = get(stateInfoById, "0.languages", []);
-  return { languages };
+  return { languages ,locale};
 };
 
 const mapDispatchToProps = (dispatch) => {
