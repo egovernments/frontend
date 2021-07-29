4b4ab881b2f3834ca18791903d6167aaa0fa0864
saumya-egov <74046603+saumya-egov@users.noreply.github.com>
2021-07-29 15:20:25 +0530

    changing the workflow url

--- web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/receipts/cancelReceiptResource/cancelReceiptFooter.js
+++ web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/receipts/cancelReceiptResource/cancelReceiptFooter.js
@@ -149,7 +149,7 @@ const cancelReceipt = async (state, dispatch) => {
       set(paymentWorkflows[0], 'paymentId', get(state.screenConfiguration.preparedFinalObject, 'PaymentReceipt.id', ''));
       let payload = await httpRequest(
         "post",
-        `${PAYMENTSEARCH.GET.URL}${getQueryArg(window.location.href, "businessService")}/_workflow`,
+        `${PAYMENTSEARCH.GET.URL}_workflow`,
         "_search",
         [],
         { paymentWorkflows: paymentWorkflows }
