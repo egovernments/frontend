595b8dea603eb8046ec47e84cd47b8fa85087a68
gurpreetsinghkhanna <45963460+gurpreetsinghkhanna@users.noreply.github.com>
2021-09-10 11:04:58 +0530

    existing property check message inrequired document header during new property

--- web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-containers/RequiredDocuments/reqDocs.js
+++ web/rainmaker/dev-packages/egov-ui-framework-core/src/ui-containers/RequiredDocuments/reqDocs.js
@@ -20,6 +20,14 @@ const styles = {
     lineHeight: "28px",
     paddingLeft: "5px"
   },
+  ptheader: {
+    color: "red",
+    fontFamily: "Roboto",
+    fontSize: "20px",
+    fontWeight: 500,
+    lineHeight: "28px",
+    paddingLeft: "5px"
+  },
   subHeader: {
     color: "gba(0, 0, 0, 0.87)",
     fontFamily: "Roboto",
@@ -50,6 +58,13 @@ const getHeader = (modulePrifx)=>{
     {
       labelName: `Required Documents-${modulePrifx}`,
       labelKey:getTransformedLocale(`${modulePrifx}_REQ_DOCS_HEADER`) 
+    },
+    {
+      style: styles.ptheader
+    },
+    {
+      labelName: "aa",
+      labelKey:"bb" 
     },
     {
       style: styles.header
