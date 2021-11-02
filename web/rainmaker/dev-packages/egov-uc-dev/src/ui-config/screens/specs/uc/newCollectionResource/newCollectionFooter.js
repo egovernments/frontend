fc30a222826208fee58fa8c4e99893a3df7b5d02
egovmis <mis.pmidc@punjab.gov.in>
2021-11-02 16:40:18 +0530

    reverted changes for echallan

--- web/rainmaker/dev-packages/egov-uc-dev/src/ui-config/screens/specs/uc/newCollectionResource/newCollectionFooter.js
+++ web/rainmaker/dev-packages/egov-uc-dev/src/ui-config/screens/specs/uc/newCollectionResource/newCollectionFooter.js
@@ -185,9 +185,8 @@ const prepareObj =(state,dispatch) =>{
     });
     set(eChallans[0],"taxPeriodFrom", convertDateToEpoch(eChallans[0].taxPeriodFrom));
     set(eChallans[0], "taxPeriodTo", convertDateToEpoch(eChallans[0].taxPeriodTo));
-    if(!eChallans[0].hasOwnProperty('address')){
+    if(eChallans[0].address.locality == null){
       set(eChallans[0], "address.locality.code", null);
-      set(eChallans[0], "address.street", null);
     } 
     // set(eChallans[0], "payer.mobileNumber", eChallans[0].citizen.mobileNumber);
     // set(eChallans[0], "payer.name",  eChallans[0].citizen.name);
