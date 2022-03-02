8bd1262cd882e6477e4a876aa81689e1f09f32e1
egovmis <mis.pmidc@punjab.gov.in>
2022-03-02 11:08:19 +0530

    changes for Zero receipt PT

--- web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/payResource/footer.js
+++ web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/payResource/footer.js
@@ -528,7 +528,7 @@ payloadReceiptDetails.Payments[0].paymentDetails[0].additionalDetails=taxheads;
             "adhoc_penalty":adhoc_penaltyT,
             "adhoc_rebate":adhoc_rebateT,
             "roundoff":roundoffT,
-            "total":element.amount
+            "total":totalT
             };
           arrearArray.push(arrearRow);
           taxArray.push(taxRow);
