dc4899ee07f3905eec6dc529125fff11e998e26a
Ayush Agarwal <Ayushagarwal@trisysit.com>
2022-03-10 16:41:36 +0530

    Cancel Receipt - If property in workflow - It should not be cancelled

--- web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/receipts/universalCollectionResources/function.js
+++ web/rainmaker/dev-packages/egov-abg-dev/src/ui-config/screens/specs/receipts/universalCollectionResources/function.js
@@ -85,6 +85,7 @@ export const searchApiCall = async (state, dispatch) => {
         receiptdate: get(Payments[i], `paymentDetails[0].receiptDate`),
         amount: get(Payments[i], `paymentDetails[0].bill.consumerCode`),
         status: get(Payments[i], `paymentStatus`),
+        WorkFlowStatus: get(Payments[i], `paymentDetails[0].bill.status`),
         paymentMode: get(Payments[i], `paymentMode`),
         businessService: get(Payments[i], `paymentDetails[0].bill.businessService`),
         tenantId: get(Payments[i], `tenantId`),
@@ -104,7 +105,7 @@ export const searchApiCall = async (state, dispatch) => {
         ['CR_COMMON_TABLE_COL_DATE']: convertEpochToDate(item.receiptdate) || "-",
         ['CR_COMMON_TABLE_CONSUMERCODE']: item.amount || "-",
         ['CR_COMMON_TABLE_COL_STATUS']: item.status || "-",
-        ['CR_COMMON_TABLE_ACTION']:(item.status!=="CANCELLED" && item.paymentMode!=="ONLINE")&&(item.instrumentStatus="APPROVED"||item.instrumentStatus=="REMITTED")&&(convertedConfig[item.businessService]?convertedConfig[item.businessService].cancelReceipt:convertedConfig['DEFAULT'].cancelReceipt)? "CANCEL":"NA",
+        ['CR_COMMON_TABLE_ACTION']:(item.status!=="CANCELLED" &&item.WorkFlowStatus==="ACTIVE" && item.paymentMode!=="ONLINE")&&(item.instrumentStatus="APPROVED"||item.instrumentStatus=="REMITTED")&&(convertedConfig[item.businessService]?convertedConfig[item.businessService].cancelReceipt:convertedConfig['DEFAULT'].cancelReceipt)? "CANCEL":"NA",
         ["RECEIPT_KEY"]: get(uiConfigs.filter(item => item.code === item.businessService), "0.receiptKey", "consolidatedreceipt"),
         ["TENANT_ID"]: item.tenantId || "-",
         ["SERVICE_TYPE"]: item.serviceType
