30c534640ceca00e19696d0868fbebc7bee097c0
egovmis <mis.pmidc@punjab.gov.in>
2022-04-01 13:05:11 +0530

    Add one field in property tax application to capture year of creation of property PI-10622

--- web/rainmaker/packages/employee/src/config/forms/specs/PropertyTaxPay/propertyAddress.js
+++ web/rainmaker/packages/employee/src/config/forms/specs/PropertyTaxPay/propertyAddress.js
@@ -10,6 +10,13 @@ import commonConfig from '../../../common'
 
 // const Search = <Icon action="action" name="home" color="#30588c" />;
 
+let floorDropDownData = [];
+
+  floorDropDownData.push({ label:"2013-14", value: "2013-14" },{ label:"2014-15", value: "2014-15" },{ label:"2015-16", value: "2015-16" },{ label:"2016-17", value: "2016-17" },{ label:"2017-18", value: "2017-18" },{ label:"2018-19", value: "2018-19" },
+  { label:"2019-20", value: "2019-20" },{ label:"2020-21", value: "2020-21" },
+  { label:"2021-22", value: "2021-22" },{ label:"2022-23", value: "2022-23" });
+
+
 const formConfig = {
   name: "propertyAddress",
   fields: {
@@ -47,7 +54,7 @@ const formConfig = {
         dataPath: ["MdmsRes.tenant.tenants"],
         dependants: [
           {
-            fieldKey: "mohalla",
+            fieldKey: "mohalla",  
           },
         ],
       },
@@ -85,7 +92,7 @@ const formConfig = {
       pattern: /^[^\$\"'<>?\\\\~`!@$%^+={}*,.:;“”‘’]{1,64}$/i,
       //toolTipMessage: "PT_OLDPID_TOOLTIP_MESSAGE",
       maxLength: 64,
-    },
+    },    
     UID: {
       id: "UID",
       type: "textFieldIcon",
@@ -102,7 +109,40 @@ const formConfig = {
       //pattern: /^[^\$\"'<>?\\\\~`!@$%^()+={}\[\]*:;“”‘’]{1,64}$/i,
       // toolTipMessage: "PT_OLDPID_TOOLTIP_MESSAGE",
       maxLength: 64,
-    }
+    },
+    
+    YearcreationProperty: {
+      id: "YearcreationProperty",
+      type: "AutocompleteDropdown",
+      className: "pt-old-pid-text-field",
+      // iconRedirectionURL: getTenantId()=='pb.amritsar'? "https://arcserver.punjab.gov.in/portal/apps/webappviewer/index.html?id=8b678d4d5020448499054bf346843ea9": getTenantId()=='pb.hoshiarpur'?"https://arcserver.punjab.gov.in/portal/apps/webappviewer/index.html?id=9bc1b255320a49c590dd17d4d258e054": "https://gis.punjab.gov.in",
+      jsonPath: "Properties[0].additionalDetails.yearConstruction",
+      floatingLabelText: "Year of creation of Property",
+      hintText: "Select",
+      numcols: 6,
+      gridDefination: {
+        xs: 12,
+        sm: 6
+      },
+      errorMessage: "PT_PROPERTY_DETAILS_PINCODE_ERRORMSG",
+      errorStyle: { position: "absolute", bottom: -8, zIndex: 5 },
+     
+      formName: "propertyAddress",
+      dropDownData: floorDropDownData,
+      updateDependentFields: ({ formKey, field, dispatch }) => {
+        if (field.value && field.value.length > 0) {
+          const mohalla = field.dropDownData.find((option) => {
+            return option.value === field.value;
+          });
+          dispatch(prepareFormData("Properties[0].additionalDetails.yearConstruction", mohalla.code));
+        }
+      },
+      // toolTip: true,
+      //pattern: /^[^\$\"'<>?\\\\~`!@$%^()+={}\[\]*:;“”‘’]{1,64}$/i,
+      // toolTipMessage: "PT_OLDPID_TOOLTIP_MESSAGE",
+      maxLength: 64,
+    },
+    
   },
   afterInitForm: (action, store, dispatch) => {
     let tenantId = getTenantId();
@@ -126,8 +166,11 @@ const formConfig = {
             cityName = tenantInfo.city.name;
 
         let surveyId=get(state.screenConfiguration.preparedFinalObject,"Properties[0].surveyId");
+        let year=get(state.screenConfiguration.preparedFinalObject,"Properties[0].additionalDetails.yearConstruction");
+        dispatch(handleFieldChange("propertyAddress", "YearcreationProperty", year));
         if(surveyId)
         dispatch(handleFieldChange("propertyAddress", "UID", surveyId));
+        
         dispatch(handleFieldChange("propertyAddress", "city", tenantId));
         dispatch(prepareFormData("Properties[0].address.city", cityName));
       }
