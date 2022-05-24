44c7ed8bd802fc7ee78ea3384b3c36a532cf097e
Ayush Agarwal <Ayushagarwal@trisysit.com>
2022-05-24 12:01:19 +0530

    changes for the user for the pt assessment

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/Property/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/Property/index.js
@@ -337,7 +337,7 @@ class Property extends Component {
 
     if(process.env.REACT_APP_NAME !='citizen'){  
       let redirectTo = `/property-tax/demand-and-collection?propertyId=${propertyId}&edit=true`;
-      if (ifUserRoleExists("PTCEMP")) {
+      if (ifUserRoleExists("PTADMIN")) {
         redirectTo = redirectTo + "&assessment=true";
       }
       this.props.history.push(redirectTo);
@@ -359,7 +359,7 @@ class Property extends Component {
     } else if(selPropertyDetails.source === "LEGACY_RECORD"){
 
       let redirectTo = `/property-tax/assessment-form-dataentry?assessmentId=0&purpose=update&propertyId=${propertyId}&tenantId=${tenantId}`;
-      if (ifUserRoleExists("PTCEMP")) {
+      if (ifUserRoleExists("PTADMIN")) {
         redirectTo = redirectTo + "&assessment=true";
       }
       this.props.history.push(redirectTo);
@@ -619,7 +619,7 @@ class Property extends Component {
               (isRoleAdmin() ||
                 Payments.length <= 0 ||
                 (Payments && Payments.length === 1 && Payments[0].instrumentStatus === "CANCELLED") ||
-                !payLen || ifUserRoleExists("PTCEMP")) && (
+                !payLen || ifUserRoleExists("PTADMIN")) && (
                 <Button
                   label={
                     <Label
@@ -636,7 +636,7 @@ class Property extends Component {
                 />
               )}
             {!isCitizen &&
-              (Payments.length <= 0 || (Payments && Payments.length === 1 && Payments[0].instrumentStatus === "CANCELLED") || !payLen || ifUserRoleExists("PTCEMP")) && (
+              (Payments.length <= 0 || (Payments && Payments.length === 1 && Payments[0].instrumentStatus === "CANCELLED") || !payLen || ifUserRoleExists("PTADMIN")) && (
                 <Button
                   onClick={() => this.editDemand()}
                   label={<Label buttonLabel={true} label="PT_EDIT_DATAENTRY_DEMAND" fontSize="16px" />}
