5cc55919d5c13a8fb3e53887dbe56dfe42970c21
Ramesh Kumar <erramesh000@gmail.com>
2022-06-30 12:21:48 +0530

    ulb or Locality

--- web/rainmaker/dev-packages/egov-pt-dev/src/ui-config/screens/specs/pt-mutation/mutation-methods.js
+++ web/rainmaker/dev-packages/egov-pt-dev/src/ui-config/screens/specs/pt-mutation/mutation-methods.js
@@ -10,7 +10,7 @@ import {
   getLabel
 } from "egov-ui-framework/ui-config/screens/specs/utils";
 import { handleScreenConfigurationFieldChange as handleField, prepareFinalObject } from "egov-ui-framework/ui-redux/screen-configuration/actions";
-import { propertySearch, applicationSearch } from "./functions";
+import { propertySearch, applicationSearch, dumm } from "./functions";
 // import "./index.css";
 
 
@@ -131,11 +131,13 @@ export const resetFields = (state, dispatch) => {
 
 };
 
-
+export const test = () =>{
+  alert("testing");
+}
 export const searchPropertyDetails = getCommonCard({
   subHeader: getCommonTitle({
-    labelName: "Search Property",
-    labelKey: "SEARCH_PROPERTY"
+    labelName: "Search Propertyjjjjj",
+    labelKey: "SEARCH_PROPERTYjjjjjj"
   }),
 
   subParagraph: getCommonParagraph({
@@ -241,7 +243,199 @@ export const searchPropertyDetails = getCommonCard({
       pattern: /^[^\$\"'<>?\\\\~`!@$%^()+={}\[\]*:;“”‘’]{1,64}$/i,
       errorMessage: "ERR_INVALID_PROPERTY_ID",
       jsonPath: "ptSearchScreen.oldpropertyids"
-    })
+    }),
+    //--------------------------
+    propertyCity: {
+      ...getSelectField({
+        label: { labelName: "City", labelKey: "NOC_PROPERTY_CITY_LABEL" },
+        localePrefix: {
+          moduleName: "TENANT",
+          masterName: "TENANTS"
+        },
+        optionLabel: "name",
+        placeholder: {
+          labelName: "Select City",
+          labelKey: "NOC_PROPERTY_CITY_PLACEHOLDER"
+        },
+        sourceJsonPath: "applyScreenMdmsData.tenant.tenants",
+        jsonPath: "FireNOCs[0].fireNOCDetails.propertyDetails.address.subDistrict",
+        required: true,
+        visible: false,
+        props: {
+          className: "applicant-details-error",
+          required: true
+          // disabled: true
+        }
+      }),
+      beforeFieldChange: async (action, state, dispatch) => {
+        //Below only runs for citizen - not required here in employee
+        dispatch(
+          prepareFinalObject(
+            "FireNOCs[0].fireNOCDetails.propertyDetails.address.subDistrict",
+            action.value
+          )
+        );
+
+        // Set Firestation based on ULBl
+        let fireStationsList = get(
+          state,
+          "screenConfiguration.preparedFinalObject.applyScreenMdmsData.firenoc.FireStations",
+          []
+        );
+        console.log("fireStationsList", fireStationsList);
+        let fireStations = fireStationsList.filter(firestation => {
+          return firestation.baseTenantId === action.value;
+        });
+        if(fireStations.length ==0){           
+          fireStations=fireStationsList.filter(firestation => {
+          let subdistrict=firestation.subDistrict;
+          subdistrict=subdistrict.filter(item=> {
+            return item.code.toUpperCase() === action.value.split('.')[1].toUpperCase();
+          });
+          if(subdistrict.length > 0)
+          return firestation;
+        });
+      }
+        // dispatch(
+        //   prepareFinalObject(
+        //     "FireNOCs[0].fireNOCDetails.firestationId", fireStations[0].code)
+        // );
+
+        let fireStationsulb = fireStationsList.filter(firestation => {
+          return firestation.ulb
+        });
+
+        let props_value;
+
+        let fire_stationid=fireStations[0].code;
+
+        dispatch(
+          prepareFinalObject(
+            "FireNOCs[0].tenantId", fireStations[0].baseTenantId)
+        );
+
+        dispatch(
+          prepareFinalObject(
+            "FireNOCs[0].fireNOCDetails.firestationId", fire_stationid)
+        );
+
+
+
+
+        try {
+          let payload = await httpRequest(
+            "post",
+            "/egov-location/location/v11/boundarys/_search?hierarchyTypeCode=REVENUE&boundaryType=Locality",
+            "_search",
+            [{ key: "tenantId", value: action.value }],
+            {}
+          );
+          console.log("payload", payload)
+          const mohallaData =
+            payload &&
+            payload.TenantBoundary[0] &&
+            payload.TenantBoundary[0].boundary &&
+            payload.TenantBoundary[0].boundary.reduce((result, item) => {
+              result.push({
+                ...item,
+                name: `${action.value
+                  .toUpperCase()
+                  .replace(
+                    /[.]/g,
+                    "_"
+                  )}_REVENUE_${item.code
+                    .toUpperCase()
+                    .replace(/[._:-\s\/]/g, "_")}`
+              });
+              return result;
+            }, []);
+
+          console.log(mohallaData, "mohallaData")
+
+
+
+          dispatch(
+            prepareFinalObject(
+              "applyScreenMdmsData.tenant.localities",
+              mohallaData
+            )
+          );
+          dispatch(
+            handleField(
+              "apply",
+              "components.div.children.formwizardSecondStep.children.propertyLocationDetails.children.cardContent.children.propertyDetailsConatiner.children.propertyMohalla",
+              "props.suggestions",
+              mohallaData
+            )
+          );
+          const mohallaLocalePrefix = {
+            moduleName: action.value,
+            masterName: "REVENUE"
+          };
+          dispatch(
+            handleField(
+              "apply",
+              "components.div.children.formwizardSecondStep.children.propertyLocationDetails.children.cardContent.children.propertyDetailsConatiner.children.propertyMohalla",
+              "props.localePrefix",
+              mohallaLocalePrefix
+            )
+          );
+
+          dispatch(
+            fetchLocalizationLabel(getLocale(), action.value, action.value)
+          );
+
+        } catch (e) {
+          console.log(e);
+        }
+
+      },
+    },
+    //-----------------------
+    
+    //--------------------------
+    locality: {
+      uiFramework: "custom-containers-local",
+      moduleName: "egov-pt",
+      componentPath: "AutosuggestContainer",
+      props: {
+        className: "autocomplete-dropdown",
+        suggestions: [],
+        label: {
+          labelName: "Locality",
+          labelKey: "Locality"
+        },
+        placeholder: {
+          labelName: "Select Locality",
+          labelKey: "Select Locality"
+        },
+        localePrefix: {
+          moduleName: "TENANT",
+          masterName: "TENANTS"
+        },
+        jsonPath: "locality.tenantId",
+        sourceJsonPath: "searchScreenMdmsData.tenant.tenants",
+        labelsFromLocalisation: true,
+        required: true,
+        isClearable: true,
+        disabled: process.env.REACT_APP_NAME === "Citizen" ? false : true,
+        inputLabelProps: {
+          shrink: true
+        }
+      },
+      required: true,
+      jsonPath: "locality.tenantId",
+      sourceJsonPath: "searchScreenMdmsData.tenant.tenants",
+      onChange: () =>{ alert("onChange"); },
+      // onClickDefination: {
+      //   action: "condition",
+      //   callBack: test
+      // },
+      gridDefination: {
+        xs: 12,
+        sm: 4
+      }
+    },
   }),
   button: getCommonContainer({
     buttonContainer: getCommonContainer({
@@ -446,6 +640,7 @@ export const searchApplicationDetails = getCommonCard({
   })
 });
 
+
 export const searchProperty = getCommonContainer({
   searchPropertyDetails,
 
@@ -454,3 +649,16 @@ export const searchProperty = getCommonContainer({
 export const searchApplication = getCommonContainer({
   searchApplicationDetails
 });
+
+
+
+
+
+
+
+
+
+
+
+
+//C:\Users\WalkingTree\Documents\frontend\web\rainmaker\dev-packages\egov-firenoc-dev\src\ui-config\screens\specs\fire-noc\applyResource\propertyLocationDetails.js
