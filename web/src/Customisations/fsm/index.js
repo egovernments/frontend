6a3555198f9b54603342868f147982eaa9ab7e1a
tarun-egov <tarun.thakur@egovernments.org>
2022-07-07 14:00:18 +0530

    door number input now taking colon

--- web/src/Customisations/fsm/index.js
+++ web/src/Customisations/fsm/index.js
@@ -1,10 +1,12 @@
 import Response from "./pages/Response";
 import FstpInbox from "./pages/employee/FstpInbox";
 import { FsmBreadCrumb } from "./pages/employee";
+import SelectStreet from "./pageComponents/SelectStreet";
 
 export const fsmComponents = {
   FSMResponse: Response,
   FSMFstpInbox: FstpInbox,
   FsmBreadCrumb: FsmBreadCrumb,
+  SelectStreet,
 };
   
