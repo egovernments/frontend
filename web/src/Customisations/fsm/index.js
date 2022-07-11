e14b96fcf9ee260140883bda8fded5e80c76d749
tarun-egov <tarun.thakur@egovernments.org>
2022-07-11 19:22:56 +0530

    corrected house number in application details

--- web/src/Customisations/fsm/index.js
+++ web/src/Customisations/fsm/index.js
@@ -2,11 +2,13 @@ import Response from "./pages/Response";
 import FstpInbox from "./pages/employee/FstpInbox";
 import { FsmBreadCrumb } from "./pages/employee";
 import SelectStreet from "./pageComponents/SelectStreet";
+import ApplicationDetails from "./pages/citizen/ApplicationDetails";
 
 export const fsmComponents = {
   FSMResponse: Response,
   FSMFstpInbox: FstpInbox,
   FsmBreadCrumb: FsmBreadCrumb,
   SelectStreet,
+  FSMCitizenApplicationDetails: ApplicationDetails,
 };
   
