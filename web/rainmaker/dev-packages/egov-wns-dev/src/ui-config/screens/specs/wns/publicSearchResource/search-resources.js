e8dccdb86f06f7decd5792b7b8ce9659f5cfa9d6
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-08-04 09:04:02 +0530

    RAIN-7353 :: Open search screen Localization issue fixed [ PC-49 , UM-1203] (#2623)

--- web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/publicSearchResource/search-resources.js
+++ web/rainmaker/dev-packages/egov-wns-dev/src/ui-config/screens/specs/wns/publicSearchResource/search-resources.js
@@ -9,7 +9,7 @@ import {
 	getLabel
 } from "egov-ui-framework/ui-config/screens/specs/utils";
 import { getLocale } from "egov-ui-kit/utils/localStorageUtils";
-import { fetchLocalizationLabel } from "egov-ui-kit/redux/app/actions";
+import { fetchLocalizationLabelForOpenScreens } from "egov-ui-kit/redux/app/actions";
 import { httpRequest } from "egov-ui-framework/ui-utils/api";
 import { searchConnections, resetFields } from "./search-methods";
 import { WithoutAuthSearchApiCall } from "../searchResource/functions"
@@ -72,7 +72,7 @@ export const searchApplications = {
 							if (index > -1) {localStorage.setItem("storedModulesList", finalList)};
 							}
 
-							dispatch(fetchLocalizationLabel(getLocale(), action.value, action.value));
+							dispatch(fetchLocalizationLabelForOpenScreens(getLocale(), action.value, action.value));
 							let payload = await httpRequest(
 								"post",
 								"/egov-location/location/v11/boundarys/_search?hierarchyTypeCode=REVENUE&boundaryType=Locality",
