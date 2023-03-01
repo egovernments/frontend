0395a78e3ce43d62c732f5c05f2fe3516d6d1e34
Ramesh Kumar <erramesh000@gmail.com>
2023-03-01 11:23:16 +0530

    FinancialYear TL 2023-24

--- web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/tradelicence/apply.js
+++ web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/tradelicence/apply.js
@@ -144,7 +144,7 @@ export const getMdmsData = async (action, state, dispatch) => {
        "MdmsRes.egf-master.FinancialYear",
        []
      ).filter(item => item.module === "TL"&& item.active === true);
-    // ).filter(item => item.module === "TL"&& item.active === true && item.code === getCurrentFinancialYear());
+     //).filter(item => item.module === "TL"&& item.active == true && item.code == getCurrentFinancialYear());
 
      set(payload, "MdmsRes.egf-master.FinancialYear",financialYearData);
     }
--- web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/tradelicence/applyResource/footer.js
+++ web/rainmaker/dev-packages/egov-tradelicence-dev/src/ui-config/screens/specs/tradelicence/applyResource/footer.js
@@ -660,7 +660,7 @@ export const renewTradelicence = async (financialYear, state, dispatch) => {
   const tenantId = get(licences[0], "tenantId");
 debugger;
   var nextFinancialYear = await getNextFinancialYearForRenewal(financialYear);
-  if(licences[0].financialYear=='2019-20' || licences[0].financialYear=='2020-21'){
+  if(licences[0].financialYear=='2019-20' || licences[0].financialYear=='2020-21' || licences[0].financialYear=='2021-22'){
     nextFinancialYear=getCurrentFinancialYear();
   }
   const wfCode = "DIRECTRENEWAL";
