644e95f5d4e022169fe3fe5668c9b9a11dcf8dab
Ramesh Kumar <erramesh000@gmail.com>
2023-03-15 11:22:13 +0530

    Fixed issues paymentDueYears PT

--- web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/AssessmentList/components/PTInformation/index.js
+++ web/rainmaker/dev-packages/egov-ui-kit-dev/src/common/propertyTax/AssessmentList/components/PTInformation/index.js
@@ -120,25 +120,27 @@ class PTInformation extends React.Component {
         ACTION: "_get",
       },
     };
-    if (purpose != PROPERTY_FORM_PURPOSE.CREATE) {
+    // if (purpose != PROPERTY_FORM_PURPOSE.CREATE) {
       const payloadProperty = await httpRequest(FETCHBILL.GET.URL, FETCHBILL.GET.ACTION, fetchBillQueryObject);
       let paymentDueYears = "";
-      if (payloadProperty.Bill != null && payloadProperty.Bill.length > 0) {
+      debugger;
+      if (payloadProperty.Bill != null && payloadProperty.Bill.length >= 0) {
+        debugger;
         payloadProperty.Bill[0].billDetails.map((item) => {
+          console.log(item.toPeriod);
+          console.log(item.fromPeriod);
           if (item.amount > 0) {
+            debugger;
             let toDate = convertEpochToDate(item.toPeriod).split("/")[2];
             let fromDate = convertEpochToDate(item.fromPeriod).split("/")[2];
-            paymentDueYears =
-              paymentDueYears == ""
-                ? fromDate + "-" + toDate + "(Rs." + item.amount + ")"
-                : paymentDueYears + "," + fromDate + "-" + toDate + "(Rs." + item.amount + ")";
+            paymentDueYears = paymentDueYears == "" ? fromDate + "-" + toDate + "(Rs." + item.amount + ")" : paymentDueYears + "," + fromDate + "-" + toDate + "(Rs." + item.amount + ")";
+           
           }
         });
       }
 
       this.setState({ paymentDueYears });
-    }
-
+   // }
     const mdmsBody = {
       MdmsCriteria: {
         tenantId: commonConfig.tenantId,
