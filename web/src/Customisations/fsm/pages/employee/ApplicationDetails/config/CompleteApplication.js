28221a044242fe6dc77c2ccf04b83cbf5e514e50
nabeelmd-eGov <nabeel.md@egovernments.com>
2023-04-04 18:38:04 +0530

    SM-2099: QA & UAT - In complete request page, Payment Received should be removed

--- web/src/Customisations/fsm/pages/employee/ApplicationDetails/config/CompleteApplication.js
+++ web/src/Customisations/fsm/pages/employee/ApplicationDetails/config/CompleteApplication.js
@@ -136,34 +136,6 @@ export const configCompleteApplication = ({
           disable: true,
           // disable: customizationConfig ? !customizationConfig?.noOfTrips?.override : true,
         },
-        {
-          label: "FSM_PAYMENT_RECEIVED",
-          isMandatory: true,
-          type: "custom",
-          populators: {
-            name: "paymentMode",
-            error: t("ES_NEW_APPLICATION_NO_OF_TRIPS_INVALID"),
-            validation: {
-              required: true,
-            },
-            rules: { required: true },
-            customProps: {
-              isMandatory: true,
-              options: receivedPaymentType,
-              optionsKey: "i18nKey",
-              innerStyles: { minWidth: "33%" },
-            },
-            component: (props, customProps) => (
-              <RadioButtons
-                selectedOption={props.value}
-                onSelect={(d) => {
-                  props.onChange(d);
-                }}
-                {...customProps}
-              />
-            ),
-          },
-        },
       ],
     },
   ],
