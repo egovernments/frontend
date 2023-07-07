802522de23d168ebb00523a573c0158b1922481f
Ramesh Kumar <erramesh000@gmail.com>
2023-07-07 09:53:02 +0530

    fixed error custom amont

--- web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/payResource/amount-to-be-paid.js
+++ web/rainmaker/dev-packages/egov-common-dev/src/ui-config/screens/specs/egov-common/payResource/amount-to-be-paid.js
@@ -41,9 +41,9 @@ const AmountToBePaid = getCommonGrayCard({
             state,
             "screenConfiguration.preparedFinalObject.ReceiptTemp[0].Bill[0]"
           );
-          if (payload && payload.totalAmount && action.value === "full_amount") {
+         // if (payload && payload.totalAmount && action.value === "full_amount") {
             dispatchHandleField(dispatch, "props.value", payload.totalAmount);
-          }
+         // }
         } catch (e) {
           console.log(e);
         }
